2dbd56d424fe18c71be1d80173eac07d3c65679b
Daniel Retzl <danielretzl@gmail.com>
2016-09-24 13:48:26 +0200

    fixed settings-toggle  ($db object was missing)

--- admin/includes/settings-toggle.php
+++ admin/includes/settings-toggle.php
@@ -1,5 +1,4 @@
 <?PHP
-global $connection;
 if ($_GET['set'] === '1') {
     $activated = 0;
 }
@@ -7,10 +6,10 @@ else {
     $activated = 1;
 }
 
-if(YAWK\settings::toggleOffline($_GET['property'], $activated)){
-    YAWK\backend::setTimeout("index.php?page=settings-backend", "0");
+if(YAWK\settings::toggleOffline($db, $_GET['property'], $activated)){
+    YAWK\backend::setTimeout("index.php?page=settings-expert", "0");
 }
 else {
-    print \YAWK\alert::draw("danger", "Error", "Could not toggle settings status.");
-    YAWK\backend::setTimeout("index.php?page=settings-backend", "0");
+    print \YAWK\alert::draw("danger", "Error", "Could not toggle settings status.", "", 3500);
+    YAWK\backend::setTimeout("index.php?page=settings-expert", "0");
 }
