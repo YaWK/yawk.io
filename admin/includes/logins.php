bfa2d38a289ca7fa9ce0cdf588eeb749064cbf78
Daniel Retzl <danielretzl@gmail.com>
2016-09-25 05:57:42 +0200

    updated login overview \admin\logins.php

--- admin/includes/logins.php
+++ admin/includes/logins.php
@@ -14,43 +14,63 @@ YAWK\backend::getTitle($lang['LOGINS'], $lang['LOGINS_SUBTEXT']);
         } );
     } );
 </script>
-
-<a class="btn btn-info" href="index.php?page=dashboard" style="float:right;" >
-    <i class="fa fa-backward"></i> &nbsp;<?php print $lang['DASHBOARD']; ?></a>
+<?php
+// TEMPLATE WRAPPER - HEADER & breadcrumbs
+echo "
+    <!-- Content Wrapper. Contains page content -->
+    <div class=\"content-wrapper\" id=\"content-FX\">
+    <!-- Content Header (Page header) -->
+    <section class=\"content-header\">";
+/* draw Title on top */
+echo \YAWK\backend::getTitle($lang['LOGINS'], $lang['LOGINS_SUBTEXT']);
+echo"<ol class=\"breadcrumb\">
+            <li><a href=\"index.php\" title=\"Dashboard\"><i class=\"fa fa-dashboard\"></i> Dashboard</a></li>
+            <li class=\"active\"><a href=\"index.php?page=logins\" title=\"Logins\"> Logins</a></li>
+         </ol>
+    </section>
+    <!-- Main content -->
+    <section class=\"content\">";
+/* page content start here */
+?>
 
 <table width="100%" cellpadding="4" cellspacing="0" border="0" class="table table-hover" id="table-sort">
     <thead>
     <tr>
         <td width="3%"><strong>&nbsp;</strong></td>
-        <td width="3%"><strong>ID</strong></td>
+        <td width="3%" class="text-center"><strong>ID</strong></td>
         <td width="15%"><strong>Datum</strong></td>
         <td width="14%" class="text-center"><strong>IP</strong></td>
-        <td width="15%"><strong>Username</strong></td>
-        <td width="10%"><strong>Password</strong></td>
-        <td width="10%"><strong>Location</strong></td>
+        <td width="15%" class="text-center"><strong>Username</strong></td>
+        <td width="10%" class="text-center"><strong>Password</strong></td>
+        <td width="10%" class="text-center"><strong>Login from</strong></td>
         <td width="2%" class="text-center"><strong>Failed</strong></td>
-        <td width="28%" style="text-align: center;"><strong>UserAgent</strong></td>
+        <td width="28%" class="text-center"><strong>Browser</strong></td>
     </tr>
     </thead>
     <tbody>
     <?PHP
-    global $dbprefix, $connection;
-    $res = mysqli_query($connection, "SELECT * FROM ".$dbprefix."logins ORDER BY id DESC");
+    // check if a user is specified via get param
+    if (isset($_GET['user']) && (!empty($_GET['user'])))
+    { $user = $_GET['user']; } else { $user = ''; } // if not, show all logins
 
-    while($row = mysqli_fetch_assoc($res)){
-
+    foreach (\YAWK\user::getLogins($db, $user) as $row)
+    {
         if ($row['failed'] === '0')
         {
             $pub = "success"; $pubtext="OK";
-            $text = "text-success";
+            $text = "text-success text-center";
         } else {
             $pub = "danger"; $pubtext = "failed";
-            $text = "text-danger";
+            $text = "text-danger text-center";
         }
         if ($row['location'] === "backend"){
-            $locationText = "text-info";
+            $locationText = "text-info text-center";
         } else {
-            $locationText = "text-muted";
+            $locationText = "text-muted text-center";
+        }
+        if (isset($row['useragent']) && (!empty($row{'useragent'})))
+        {
+            $row['useragent'] = \YAWK\sys::getBrowser($row['useragent']);
         }
 
         echo "<tr class=\"".$text."\" >
@@ -63,7 +83,7 @@ YAWK\backend::getTitle($lang['LOGINS'], $lang['LOGINS_SUBTEXT']);
                 <td class=\"".$text."\">".$row['password']."</td>
                 <td class=\"".$locationText."\">".$row['location']."</td>
                 <td class=\"".$text."\">".$row['failed']."</td>
-                <td style=\"text-align:center;\">".$row['useragent']."</td>
+                <td style=\"text-align:center;\">".$row['useragent']['name']." <small>".$row['useragent']['version']."</small></td>
 
               </tr>";
     }
