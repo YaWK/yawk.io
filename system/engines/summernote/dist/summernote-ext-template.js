0120a38b3488a380520387d8617979724542d2c9
Daniel Retzl <danielretzl@gmail.com>
2016-10-03 06:28:43 +0200

    fix: updated summernote editor: added cleaner plugin; This fixes code (line height blahbla) troubles when copy/pasting. Copy / paste works like a charm now.

---
+++ system/engines/summernote/dist/summernote-ext-template.js
@@ -0,0 +1,80 @@
+(function (factory) {
+    /* global define */
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(window.jQuery);
+    }
+}(function ($) {
+    $.extend($.summernote.options, {
+        template: {
+            list: []
+        }
+    });
+
+    // Extends plugins for adding templates.
+    //  - plugin is external module for customizing.
+    $.extend($.summernote.plugins, {
+        /**
+         * @param {Object} context - context object has status of editor.
+         */
+        'template': function (context) {
+            // ui has renders to build ui elements.
+            //  - you can create a button with `ui.button`
+            var ui      = $.summernote.ui;
+            var options = context.options.template;
+            var defaultOptions = {
+                label: "Template",
+                tooltip: "Insert Template"
+            };
+
+            // Assign default values if not supplied
+            for (var propertyName in defaultOptions) {
+                if (options.hasOwnProperty(propertyName) === false) {
+                    options[propertyName] = defaultOptions[propertyName];
+                }
+            }
+
+            // add hello button
+            context.memo('button.template', function () {
+                // create button
+                var button = ui.buttonGroup([
+                    ui.button({
+                        className: 'dropdown-toggle',
+                        contents: '<span class="template"/> ' + options.label + ' <span class="caret"></span>',
+                        tooltip: options.tooltip,
+                        data: {
+                            toggle: 'dropdown'
+                        }
+                    }),
+                    ui.dropdown({
+                        className: 'dropdown-template',
+                        items: options.list,
+                        click: function (event) {
+                            var $button = $(event.target);
+                            var value   = $button.data('value');
+                            var path    = options.path + '/' + value + '.html';
+
+                            $.get(path)
+                                .done(function (data) {
+                                    var node = document.createElement('span');
+                                    node.innerHTML = data;
+                                    context.invoke('editor.insertNode', node);
+                                }).fail(function () {
+                                alert('template not found in ' + path);
+                            });
+                        }
+                    })
+                ]);
+
+                // create jQuery object from button instance.
+                return button.render();
+            });
+        }
+    });
+}));
