5ac84c48502ef72ee11b6c5492f6054068c6de95
Daniel Retzl <danielretzl@gmail.com>
2016-10-06 08:45:58 +0200

    add feature: lineNumbers will now be displayed in editor's codeview (codemirror)

--- admin/includes/page-edit.php
+++ admin/includes/page-edit.php
@@ -89,73 +89,94 @@ $page->alias = preg_replace("/[^a-z0-9\-\/]/i","",$alias); // final check: just 
 <script src="../system/engines/summernote/dist/summernote-cleaner.js"></script>
 <script src="../system/engines/summernote/dist/summernote-image-attributes.js"></script>
 <script src="../system/engines/summernote/dist/summernote-floats-bs.js"></script>
+
 <script type="text/javascript">
 $(document).ready(function() {
-
+    // textarea that will be transformed into editor
+    var editor = ('textarea#summernote');
+    // ok, lets go...
+    // we need to check if user clicked on save button
         $( "#savebutton" ).click(function() {
-            // check if codeview is active
-            if ($('textarea#summernote').summernote('codeview.isActivated')) {
-                // if so, turn it off
-                $('textarea#summernote').summernote('codeview.deactivate');
+            // to save, even if the editor is currently opened in code view
+            // we need to check if codeview is currently active:
+            if ($(editor).summernote('codeview.isActivated')) {
+                // if so, turn it off.
+                $(editor).summernote('codeview.deactivate');
             }
+            // to display images in frontend correctly, we need to change the path of every image.
+            // to do that, the current value of textarea will be read into var text and search/replaced
+            // and written back into the textarea. utf-8 encoding/decoding happens in php, before saving into db.
             // get the value of summernote textarea
-            var text = $('textarea#summernote').val();
+            var text = $(editor).val();
             // search for <img> tags and revert src ../ to set correct path for frontend
             var frontend = text.replace(/<img src=\"..\/media/g,"<img src=\"media");
             // put the new string back into <textarea>
-            $('textarea#summernote').val(frontend); // to make sure that saving works
+            $(editor).val(frontend); // to make sure that saving works
         });
 
-    // ## SUMMERNOTE HACK
+    // BEFORE SUMMERNOTE loads: 3 important lines of code!
+    // to display images in backend correctly, we need to change the path of every image.
+    // procedure is the same as above (see #savebutton.click)
     // get the value of summernote textarea
-    var text = $('textarea#summernote').val();
+    var text = $(editor).val();
     // search for <img> tags and update src ../ to get images viewed in summernote
     var backend = text.replace(/<img src=\"media/g,"<img src=\"../media");
     // put the new string back into <textarea>
-    $('textarea#summernote').val(backend); // set new value into textarea
+    $(editor).val(backend); // set new value into textarea
 
-    // initialize summernote editor
-    $('#summernote').summernote({
+    // INIT SUMMERNOTE EDITOR
+    $('#summernote').summernote({    // set editor itself
         height: 420,                 // set editor height
         minHeight: null,             // set minimum height of editor
         maxHeight: null,             // set maximum height of editor
         focus: true,                 // set focus to editable area after initializing summernote
 
+        // popover tooltips
         popover: {
             image: [
                 ['imagesize', ['imageSize100', 'imageSize50', 'imageSize25']],
-                /* ['float', ['floatLeft', 'floatRight', 'floatNone']], */
-                /* Those are the old regular float buttons */
-                ['floatBS', ['floatBSLeft', 'floatBSNone', 'floatBSRight']],
-                ['custom', ['imageAttributes', 'imageShape']],
+                /* ['float', ['floatLeft', 'floatRight', 'floatNone']], // those are the old regular float buttons */
+                ['floatBS', ['floatBSLeft', 'floatBSNone', 'floatBSRight']],    // bootstrap class buttons (float/pull)
+                ['custom', ['imageAttributes', 'imageShape']], // forked plugin: image-attributes.js
                 ['remove', ['removeMedia']]
             ]
         },
+        // language for plugin image-attributes.js
         lang: '<?php echo $_SESSION['lang']; ?>',
 
+        // powerup the codeview with codemirror theme
         codemirror: { // codemirror options
-            theme: 'monokai'
+            theme: 'monokai',    // ya know why? - best for long sessions
+            lineNumbers: true,
+            undoDepth: 200,
+            scrollbarStyle: null
         },
 
-        cleaner: {
+        // plugin: summernote-cleaner.js
+        // this allows to copy/paste from word, browsers etc.
+        cleaner: { // does the job well: no messy code anymore!
             action: 'both', // both|button|paste 'button' only cleans via toolbar button, 'paste' only clean when pasting content, both does both options.
             newline: '<br>' // Summernote's default is to use '<p><br></p>'
-            // notTime:2400, // Time to display Notifications.
-            // notStyle:'position:absolute;bottom:0;left:2px', // Position of Notification
-            // icon:'<i class="note-icon">[Your Button]</i>'
+
+            // silent mode:
+            // from my pov it is not necessary to inform the user about the sourcecode cleaning process.
+            // just a useless, annoying bubble: everytime you hit the save button.
+            // But if you need this notification, you can enable it by uncommenting the following 3 lines
+            // notTime:2400,                                            // Time to display notifications.
+            // notStyle:'position:absolute;bottom:0;left:2px',          // Position of notification
+            // icon:'<i class="note-icon">[Your Button]</i>'            // Display an icon
         }
     }); // end summernote
-
 }); // end document ready
 </script>
 
 <script type="text/javascript" >
 $(document).ready(function() {
-
+// load datetimepicker  (start time)
 $('#datetimepicker1').datetimepicker({
     format: 'yyyy-mm-dd hh:ii:ss'
 });
-
+// load 2nd datetimepicker (end time)
 $('#datetimepicker2').datetimepicker({
     format: 'yyyy-mm-dd hh:ii:ss'
 });
