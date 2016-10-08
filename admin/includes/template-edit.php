1ffcab754e43ba199243a80bfa96671e74327872
Daniel Retzl <danielretzl@gmail.com>
2016-10-08 23:41:26 +0200

    feature: added the perfect codemirror to custom CSS textarea

--- admin/includes/template-edit.php
+++ admin/includes/template-edit.php
@@ -29,7 +29,125 @@
         });
     });
 </script>
+<?php
+// get settings for editor
+$editorSettings = \YAWK\settings::getEditorSettings($db, 14);
+?>
+<!-- include summernote css/js-->
+<!-- include codemirror (codemirror.css, codemirror.js, xml.js) -->
+<link rel="stylesheet" type="text/css" href="../system/engines/codemirror/codemirror.min.css">
+<link rel="stylesheet" type="text/css" href="../system/engines/codemirror/themes/<?php echo $editorSettings['editorTheme']; ?>.css">
+<link rel="stylesheet" type="text/css" href="../system/engines/codemirror/show-hint.min.css">
+<script type="text/javascript" src="../system/engines/codemirror/codemirror-compressed.js"></script>
+<script type="text/javascript" src="../system/engines/codemirror/auto-refresh.js"></script>
 
+<!-- SUMMERNOTE -->
+<link href="../system/engines/summernote/dist/summernote.css" rel="stylesheet">
+<script src="../system/engines/summernote/dist/summernote.min.js"></script>
+<script src="../system/engines/summernote/dist/summernote-cleaner.js"></script>
+<script src="../system/engines/summernote/dist/summernote-image-attributes.js"></script>
+<script src="../system/engines/summernote/dist/summernote-floats-bs.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        // textarea that will be transformed into editor
+        var editor = ('textarea#summernote');
+        // ok, lets go...
+        // we need to check if user clicked on save button
+        $( "#savebutton" ).click(function() {
+            // to save, even if the editor is currently opened in code view
+            // we need to check if codeview is currently active:
+            if ($(editor).summernote('codeview.isActivated')) {
+                // if so, turn it off.
+                $(editor).summernote('codeview.deactivate');
+            }
+            // to display images in frontend correctly, we need to change the path of every image.
+            // to do that, the current value of textarea will be read into var text and search/replaced
+            // and written back into the textarea. utf-8 encoding/decoding happens in php, before saving into db.
+            // get the value of summernote textarea
+            var text = $(editor).val();
+            // search for <img> tags and revert src ../ to set correct path for frontend
+            var frontend = text.replace(/<img src=\"..\/media/g,"<img src=\"media");
+            // put the new string back into <textarea>
+            $(editor).val(frontend); // to make sure that saving works
+        });
+
+        // BEFORE SUMMERNOTE loads: 3 important lines of code!
+        // to display images in backend correctly, we need to change the path of every image.
+        // procedure is the same as above (see #savebutton.click)
+        // get the value of summernote textarea
+        var text = $(editor).val();
+        // search for <img> tags and update src ../ to get images viewed in summernote
+        var backend = text.replace(/<img src=\"media/g,"<img src=\"../media");
+        // put the new string back into <textarea>
+        $(editor).val(backend); // set new value into textarea
+
+        // summernote.init
+        $(editor).on('summernote.init', function() {
+            // toggle editor to codeview
+            $(editor).summernote('codeview.toggle');
+        });
+
+        // INIT SUMMERNOTE EDITOR
+        $(editor).summernote({    // set editor itself
+            height: <?php echo $editorSettings['editorHeight']; ?>,                 // set editor height
+            minHeight: null,             // set minimum height of editor
+            maxHeight: null,             // set maximum height of editor
+            focus: true,                 // set focus to editable area after initializing summernote
+            toolbar: [
+                // load an empty toolbar.
+                // in that case: just a plain code editor. no mice cinema.
+            ],
+            // popover tooltips
+            popover: {
+                image: [
+                    ['imagesize', ['imageSize100', 'imageSize50', 'imageSize25']],
+                    /* ['float', ['floatLeft', 'floatRight', 'floatNone']], // those are the old regular float buttons */
+                    ['floatBS', ['floatBSLeft', 'floatBSNone', 'floatBSRight']],    // bootstrap class buttons (float/pull)
+                    ['custom', ['imageAttributes', 'imageShape']], // forked plugin: image-attributes.js
+                    ['remove', ['removeMedia']]
+                ]
+            },
+            // language for plugin image-attributes.js
+            lang: '<?php echo $_SESSION['lang']; ?>',
+
+            // powerup the codeview with codemirror theme
+            codemirror: { // codemirror options
+                theme: '<?php echo $editorSettings['editorTheme']; ?>',                       // codeview theme
+                lineNumbers: true,             // display lineNumbers true|false
+                undoDepth: <?php echo $editorSettings['editorUndoDepth']; ?>,                 // how many undo steps should be saved? (default: 200)
+                smartIndent: <?php echo $editorSettings['editorSmartIndent']; ?>,             // better indent
+                indentUnit: <?php echo $editorSettings['editorIndentUnit']; ?>,               // how many spaces auto indent? (default: 2)
+                scrollbarStyle: null,                                                         // styling of the scrollbars
+                matchBrackets: <?php echo $editorSettings['editorMatchBrackets']; ?>,         // highlight corresponding brackets
+                autoCloseBrackets: <?php echo $editorSettings['editorCloseBrackets'];?>,      // auto insert close brackets
+                autoCloseTags: <?php echo $editorSettings['editorCloseTags']; ?>,             // auto insert close tags after opening
+                value: "<html>\n  " + document.documentElement.innerHTML + "\n</html>",       // all html
+                mode: "css",                                                            // editor mode
+                matchTags: {bothTags: <?php echo $editorSettings['editorMatchTags']; ?>},     // hightlight matching tags: both
+                extraKeys: {"Ctrl-J": "toMatchingTag", "Ctrl-Space": "autocomplete"},         // press ctrl-j to jump to next matching tab
+                styleActiveLine: <?php echo $editorSettings['editorActiveLine']; ?>,           // highlight the active line (where the cursor is)
+                autoRefresh: true
+            },
+
+            // plugin: summernote-cleaner.js
+            // this allows to copy/paste from word, browsers etc.
+            cleaner: { // does the job well: no messy code anymore!
+                action: 'both', // both|button|paste 'button' only cleans via toolbar button, 'paste' only clean when pasting content, both does both options.
+                newline: '<br>' // Summernote's default is to use '<p><br></p>'
+
+                // silent mode:
+                // from my pov it is not necessary to notify the user about the code cleaning process.
+                // it throws just a useless, annoying bubble everytime you hit the save button.
+                // BUT: if you need this notification, you can enable it by uncommenting the following 3 lines
+                // notTime:2400,                                            // Time to display notifications.
+                // notStyle:'position:absolute;bottom:0;left:2px',          // Position of notification
+                // icon:'<i class="note-icon">[Your Button]</i>'            // Display an icon
+            }
+        }); // end summernote
+
+    }); // end document ready
+</script>
 <?php
     // new template object if not exists
     if (!isset($template)) { $template = new \YAWK\template(); }
@@ -1414,7 +1532,7 @@ else
                 <div class="col-md-8">
                     <label class="h3" for="summernote">Custom.CSS
                         <small>Override settings and add your own definitions to extend this template</small></label>
-                    <textarea name="customCSS" cols="64" rows="28" id="summernote" class="form-control dark"><?php
+                    <textarea name="customCSS" cols="64" rows="28" id="summernote"><?php
                         $customCSS = $template->getCustomCSSFile($db, $template->id);
                         echo $customCSS; ?></textarea>
                 </div>
