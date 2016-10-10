506b2b71c06c4dcff6e5b908e020416c678acc2a
Daniel Retzl <danielretzl@gmail.com>
2016-10-10 06:17:11 +0200

    feature: add modal window JS to yawk-backend.js

--- admin/js/yawk-backend.js
+++ admin/js/yawk-backend.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+    /* modal dialog data-confirm */
+    $('a[data-confirm]').click(function(ev) {
+        modal = '#dataConfirmModal';
+        var href = $(this).attr('href');
+        if (!$(modal).length) {
+            $('body').append('<div id="dataConfirmModal" class="modal fade" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i></button><h3 id="dataConfirmLabel"><i style="color:#f0ad4e;" class="fa fa-warning"></i> Achtung! <small>Bist Du sicher?</small></h3></div><div class="modal-body"></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal" aria-hidden="true">Abbrechen</button><a type="button" class="btn btn-danger" id="dataConfirmOK"> <i class="fa fa-trash-o"></i> L&ouml;schen</a></div></div></div></div>');
+        }
+        $(modal).find('.modal-body').text($(this).attr('data-confirm'));
+        $('#dataConfirmOK').attr('href', href);
+        $(modal).modal({show:true});
+        return false;
+    });
+
     // NOTIFICATION DISMISS BTN
     $("#dismiss").click(function()
     {   // fade away the orange label on top
