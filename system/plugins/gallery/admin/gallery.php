15e454c537f42b45a41aef1c0f0817be392d6e92
Daniel Retzl <danielretzl@gmail.com>
2016-10-27 08:07:20 +0200

    add preview to \YAWK\PLUGINS\GALLERY\

--- system/plugins/gallery/admin/gallery.php
+++ system/plugins/gallery/admin/gallery.php
@@ -5,6 +5,21 @@ if (!isset($gallery))
 {
     $gallery = new \YAWK\PLUGINS\GALLERY\gallery();
 }
+// ADD NEW GALLERY
+if (isset($_POST))
+{
+    if (isset($_GET['delete']) && ($_GET['delete'] === "1"))
+    {   // delete a gallery from database
+        if ($gallery->delete($db) == true)
+        {   // deletion successfull
+            \YAWK\alert::draw("success", "Gallery deleted successfully.", "Files on disk are <i>not</i> affected.", "", 1200);
+        }
+    }
+    if (isset($_GET['add']) && ($_GET['add'] === "1"))
+    {   // add a new gallery to database
+        $gallery->add($db);
+    }
+}
 ?>
 <script type="text/javascript">
     $(document).ready(function () {
@@ -30,13 +45,15 @@ echo \YAWK\backend::getTitle($lang['GALLERY'], $lang['GALLERY_SUBTEXT']);
 echo"<ol class=\"breadcrumb\">
             <li><a href=\"index.php\" title=\"Dashboard\"><i class=\"fa fa-dashboard\"></i> Dashboard</a></li>
             <li><a href=\"index.php?page=plugins\" title=\"Plugins\"> Plugins</a></li>
-            <li class=\"active\"><a href=\"index.php?plugin=blog\" title=\"Blog\"> Blog</a></li>
+            <li class=\"active\"><a href=\"index.php?plugin=gallery\" title=\"Gallery\"> Gallery</a></li>
          </ol>
     </section>
     <!-- Main content -->
     <section class=\"content\">";
 /* page content start here */
 ?>
+
+<form action="index.php?plugin=gallery&pluginpage=gallery&add=1" role="form" method="POST">
 <div class="row">
     <div class="col-md-6">
         <div class="box box-default">
@@ -44,19 +61,47 @@ echo"<ol class=\"breadcrumb\">
                 <h3 class="box-title">Add a new gallery</h3>
             </div>
             <div class="box-body">
-                <label for="path2file">Folder where files are located</label>
+                <label for="folder">Select the folder where your images are located</label>
                 <?php echo $gallery->drawFolderSelect("../media/images/")?>
-                <input id="path2file" type="text" placeholder="media/images/your-images-folder" class="form-control">
+                <label for="customFolder">or set any different folder</label>
+                <input id="customFolder"
+                       name="customFolder"
+                       type="text"
+                       placeholder="media/images/your-images-folder"
+                       class="form-control">
+                <label for="title">Gallery Title</label>
+                <input id="title"
+                       name="title"
+                       type="text"
+                       placeholder="What is your gallery about?"
+                       class="form-control">
+                <label for="description">Gallery Description <small>(optional)</small></label>
+                <input id="description"
+                       name="description"
+                       type="text"
+                       placeholder="Description (can be displayed before the gallery)"
+                       class="form-control"><br>
+                <!-- SAVE BUTTON -->
+                <button type="submit"
+                        id="savebutton"
+                        name="save"
+                        class="btn btn-success pull-right">
+                    <i id="savebuttonIcon" class="fa fa-check"></i> &nbsp;<?php print $lang['GALLERY_ADD']; ?>
+                </button>
             </div>
         </div>
     </div>
     <div class="col-md-6">
         <div class="box box-default">
+            <div class="box-header">
+                <h3 class="box-title">Existing Galleries</h3>
+            </div>
             <div class="box-body">
-                <?php echo $gallery->scanDir("../media/images/"); ?>
+                <?php echo $gallery->getPreview($db, $lang); ?>
             </div>
         </div>
     </div>
 </div>
+</form>
 
 
--- system/plugins/gallery/classes/gallery.php
+++ system/plugins/gallery/classes/gallery.php
@@ -1,15 +1,25 @@
 <?php
 namespace YAWK\PLUGINS\GALLERY {
 
+    use YAWK\filemanager;
+
     class gallery
     {
+        public $id;
+        public $folder;
+        public $title;
+        public $description;
+        public $images;
+
+
         public function __construct(){
         // include 'system/plugins/messages/classes/messages.php';
         }
 
         public function drawFolderSelect($path)
         {
-            echo "<select name=\"newFolder\" class=\"form-control\" id=\"newFolder\">
+            echo "<select name=\"folder\" class=\"form-control\" id=\"folder\">
+                  <option value=\"Select Image Folder\">Select Image Folder</option>
                   ".self::scanDir($path)."
                   </select>";
         }
@@ -26,15 +36,159 @@ namespace YAWK\PLUGINS\GALLERY {
 
                 if ($file->isDir()) {
                     // print $file->getFilename() . '<br>';
-                    $html .= "<option value=\"".$file->getFilename()."\">".$file->getFilename()."</option>";
+                    $html .= "<option value=\"$path".$file->getFilename()."\">images/".$file->getFilename()."</option>";
                 }
             }
             return $html;
         }
 
+
         public function init()
         {
             // do something...
         }
+
+        public function delete($db)
+        {   /** @var $db \YAWK\db **/
+            // delete a gallery
+            if (isset($_GET['id']) && (!empty($_GET['id']) && (is_numeric($_GET['id']))))
+            {   // delete gallery from database
+                if ($res = $db->query("DELETE FROM {plugin_gallery} WHERE id = '$_GET[id]'"))
+                {   // gallery deleted...
+                    // now go ahead with the items
+                    if (!$deleteItems = $db->query("DELETE FROM {plugin_gallery_items} 
+                                                           WHERE galleryID = '$_GET[id]'"))
+                    {   // could not delete items
+                        \YAWK\alert::draw("warning", "Could not delete gallery items from database", "Please try again!", "", 5800);
+                    }
+
+                    // ALTER table and set auto_increment value to prevent errors when deleting + adding new tpl
+                    if ($res = $db->query("SELECT MAX(id) FROM {plugin_gallery}"))
+                    {   // get MAX ID
+                        $row = mysqli_fetch_row($res);
+                        if (!$res = $db->query("ALTER TABLE {plugin_gallery} AUTO_INCREMENT $row[0]"))
+                        {   // could not delete plugin_gallery
+                            return false;
+                        }
+                    }
+                    else
+                        {   // could not get maxID, maybe there is not entry - reset auto increment value to zero
+                            if (!$res = $db->query("ALTER TABLE {plugin_gallery} AUTO_INCREMENT 0"))
+                            {   // could not select auto encrement
+                                return false;
+                            }
+                        }
+                }
+                else
+                    {   // delete failed, throw error
+                        \YAWK\alert::draw("danger", "Could not delete this gallery", "Please try again!", "", 5800);
+                    }
+            }
+            return true;
+        }
+
+        public function reScanFolder($db, $folder)
+        {   /** @var $db \YAWK\db **/
+            // delete a gallery
+
+        }
+
+        public function add($db)
+        {   /** @var $db \YAWK\db **/
+            // add a new gallery
+            if (isset($_POST['folder']) && (!empty($_POST['folder'])))
+            {   // gallery folder
+                $this->folder = $db->quote($_POST['folder']);
+            }
+            if (isset($_POST['customFolder']) && (!empty($_POST['customFolder'])))
+            {   // if a custom folder is set, overwrite gallery folder
+                $this->folder = $_POST['customFolder'];
+            }
+            if (isset($_POST['title']) && (!empty($_POST['title'])))
+            {   // gallery title
+                $this->title = $db->quote($_POST['title']);
+            }
+            if (isset($_POST['description']) && (!empty($_POST['description'])))
+            {   // gallery description
+                $this->description = $db->quote($_POST['description']);
+            }
+
+            // add new gallery to database
+            if ($res = $db->query("INSERT INTO {plugin_gallery} (folder, title, description)
+                        VALUES ('" . $this->folder . "','" . $this->title . "','" . $this->description . "')"))
+            {   // all good
+                \YAWK\alert::draw("success", "Gallery created.", "Database entry success.", "", 800);
+            }
+            else
+            {   // gallery could not be added - notify user
+                \YAWK\alert::draw("danger", "Could not add the new gallery.", "Please check your data and try again.", "", 5800);
+            }
+
+            // get ID from current added gallery
+            if ($res = $db->query("SELECT id from {plugin_gallery} WHERE folder = '$this->folder'"))
+            {   // get last ID from db
+                $row = mysqli_fetch_row($res);
+                $galleryID = $row[0];
+            }
+            else
+                {   // if ID cannot be found, use the gallery title as identifier
+                    $galleryID = $this->title;
+                }
+
+            // iterate trough folder and save each file in a db row...
+            foreach (new \DirectoryIterator($this->folder) as $fileInfo) {
+                if($fileInfo->isDot()) continue;
+                if($fileInfo->isDir()) continue;
+                $filename = $fileInfo->getFilename();
+                // TODO: this needs to be improved:
+                // TODO: 1 db insert per file is NOT! ok - but how to implement implode() correctly to avoid that memory lack?
+                if ($res = $db->query("INSERT INTO {plugin_gallery_items} (galleryID, filename, title, author, authorUrl)
+                        VALUES ('" . $galleryID . "','" . $filename . "','" . $filename . "','" . " " . "', '". " " ."')"))
+                {   // all good
+                    // \YAWK\alert::draw("success", "Gallery created.", "Database entry success.", "", 800);
+                }
+                else
+                    {   // error inserting data, throw notification
+                        \YAWK\alert::draw("warning", "Could not insert $filename", "Database error. Please check folder and try again.", "", 1200);
+                    }
+            }
+            return true;
+        }
+
+        public function getPreview($db, $lang)
+        {   /** @var $db \YAWK\db **/
+            // get gallery titles...
+            if ($res = $db->query("SELECT * from {plugin_gallery}"))
+            {
+                while ($row = mysqli_fetch_assoc($res))
+                {
+                    if (!$getPreviewImages = $db->query("SELECT galleryID, filename from {plugin_gallery_items} WHERE galleryID = '$row[id]'"))
+                    {   // store info msg, if files could not be retrieved
+                        $previewError = "Sorry, no preview available.";
+                    }
+                    // preview without images
+                    echo "<div class=\"row\"><div class=\"col-md-4\"><a class=\"fa fa-trash-o\" role=\"dialog\" data-confirm=\"Soll die Galerie &laquo;" . $row['id'] . " / " . $row['title'] . "&raquo; wirklich gel&ouml;scht werden?\"
+                      title=\"" . $lang['DEL'] . "\" href=\"index.php?plugin=gallery&delete=1&id=" . $row['id'] . "\"></a>
+                      &nbsp;<a href=\"#\"><i class=\"fa fa-refresh\"></i></a>
+                      &nbsp;<b>".$row['title']."</b><br><small>".$row['description']."</small></div>
+                    <div class=\"col-md-8\">";
+                    if (isset($previewError))
+                    {   // if files could not be loaded from db
+                        echo $previewError;
+                    }
+                    else
+                    {   // previewImage array is set, walk through it...
+                        foreach ($getPreviewImages as $property => $image)
+                        {   // display preview images
+                            echo "<img src=\"$row[folder]/$image[filename]\" class=\"img-thumbnail\" width=\"100\">";
+                        }
+                    }
+                    echo"</div></div>
+                    <hr>";
+                }
+            }
+            return null;
+        }
+
     }
 }
