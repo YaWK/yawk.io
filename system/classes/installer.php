cfabb1a424c291a874c0fca1cf46c2d5a2834c07
Daniel Retzl <danielretzl@gmail.com>
2016-11-16 20:11:34 +0100

    added classes\installer.php

---
+++ system/classes/installer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace YAWK {
+
+    /**
+     * Class installer
+     * @package YAWK
+     */
+    class installer
+    {
+        /**
+         * @var $config
+         */
+        private $config;
+
+        /**
+         * Check if installation is needed
+         *
+         */
+        function checkForInstallation()
+        {
+
+        }
+    }
+}
