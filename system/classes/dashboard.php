4a3cd3a1ccffdb55a33db94ec02cd94bd2c7e5ae
Daniel Retzl <danielretzl@gmail.com>
2016-11-21 17:08:01 +0100

    updated dashboard class

--- system/classes/dashboard.php
+++ system/classes/dashboard.php
@@ -14,7 +14,7 @@ namespace YAWK
             echo "<!-- PRODUCT LIST -->
         <div class=\"box box-primary\">
             <div class=\"box-header with-border\">
-                <h3 class=\"box-title\">Recently Added Pages</h3>
+                <h3 class=\"box-title\">Recently Added Static Pages</h3>
 
                 <div class=\"box-tools pull-right\">
                     <button type=\"button\" class=\"btn btn-box-tool\" data-widget=\"collapse\"><i class=\"fa fa-minus\"></i>
