eec6abbeab2ba9dc9c534f21c8278966ad61c559
Daniel Retzl <danielretzl@gmail.com>
2016-11-22 05:20:34 +0100

    added function sys::getOS()

--- system/classes/sys.php
+++ system/classes/sys.php
@@ -353,6 +353,12 @@ namespace YAWK {
             elseif (preg_match('/windows|win32/i', $u_agent)) {
                 $platform = 'windows';
             }
+            elseif(preg_match('/linux|android/i',$u_agent))
+            {
+                $bname = 'Android';
+                $ub = "Android";
+                $platform = "Android";
+            }
 
             // Next get the name of the useragent yes seperately and for good reason
             if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent))
@@ -426,6 +432,49 @@ namespace YAWK {
                 'platform'  => $platform,
                 'pattern'    => $pattern
             );
+        }
+
+        static function getOS($useragent) {
+            if (isset($useragent) && (!empty($useragent)))
+            {
+                $u_agent = $useragent;
+            }
+            else
+            {
+                $u_agent = $_SERVER['HTTP_USER_AGENT'];
+            }
+
+            $os_platform    =   "Unknown OS Platform";
+            $os_array       =   array(
+                '/windows nt 6.2/i'     =>  'Windows 8',
+                '/windows nt 6.1/i'     =>  'Windows 7',
+                '/windows nt 6.0/i'     =>  'Windows Vista',
+                '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                '/windows nt 5.1/i'     =>  'Windows XP',
+                '/windows xp/i'         =>  'Windows XP',
+                '/windows nt 5.0/i'     =>  'Windows 2000',
+                '/windows me/i'         =>  'Windows ME',
+                '/win98/i'              =>  'Windows 98',
+                '/win95/i'              =>  'Windows 95',
+                '/win16/i'              =>  'Windows 3.11',
+                '/macintosh|mac os x/i' =>  'Mac OS X',
+                '/mac_powerpc/i'        =>  'Mac OS 9',
+                '/linux/i'              =>  'Linux',
+                '/ubuntu/i'             =>  'Ubuntu',
+                '/iphone/i'             =>  'iPhone',
+                '/ipod/i'               =>  'iPod',
+                '/ipad/i'               =>  'iPad',
+                '/android/i'            =>  'Android',
+                '/blackberry/i'         =>  'BlackBerry',
+                '/webos/i'              =>  'Mobile'
+            );
+
+            foreach ($os_array as $regex => $value) {
+                if (preg_match($regex, $u_agent)) {
+                    $os_platform    =   $value;
+                }
+            }
+            return $os_platform;
         }
 
         static function getLoadingTime($debugTime)
