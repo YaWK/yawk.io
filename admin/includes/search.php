00a6c3bc850fe043d491c6c5b8b3b4d2194a3ce8
Daniel Retzl <danielretzl@gmail.com>
2017-02-17 04:56:08 +0100

    fixed issue #20 - added basic backend search

--- admin/includes/search.php
+++ admin/includes/search.php
@@ -18,47 +18,103 @@ echo"<ol class=\"breadcrumb\">
 ?>
 
 <?php
-// include stats class
-require_once '../system/classes/stats.php';
-// check if stats object is set...
-if (!isset($stats))
-{   // if not, create new
-    $stats = new \YAWK\stats();
-}
-// load stats data into an array that every box will use, this saves performance
-$data = $stats->getStatsArray($db);
-
-if (isset($_POST['limit']) && (!empty($_POST['limit'])))
+// include search class
+require_once '../system/classes/search.php';
+if (!isset($search) || (empty($search)))
 {
-    $limit = strip_tags($_POST['limit']);
+    $search = new \YAWK\search();
+}
+if (!isset($db) || (empty($db)))
+{
+    require_once '../system/classes/db.php';
+    $db = new \YAWK\db();
+}
+
+if (isset($_POST['searchString']) && (!empty($_POST['searchString'])))
+{
+    $search->string = strip_tags($_POST['searchString']);
 }
 else
 {
-    $limit = $stats->i_hits;
+    $search->string = '';
 }
 ?>
+<form method="post" action="index.php?page=search">
 <div class="row">
     <div class="col-md-8">
-        <!-- box -->
+        <!-- search box -->
         <div class="box">
             <div class="box-header with-border">
-                <h3 class="box-title"><?php echo $lang['RESULT'] ?> <small><?php echo $lang['ALL_ELEMENTS']; ?></small></h3>
+                <h3 class="box-title"><?php echo $lang['SEARCH_STRING'] ?> <small><?php echo $lang['NEW_SEARCH']; ?></small></h3>
             </div>
             <div class="box-body">
-              ...
+                <input id="searchString" name="searchString" type="text" value="<?php echo $search->string; ?>" class="form-control h3" placeholder="<?php echo $search->string; ?>">
             </div>
         </div>
-        <!-- / box -->
+        <!-- / search box -->
+
+        <!-- pages results box -->
+            <?php echo $search->searchPages($db, $search->string, $lang); ?>
+        <!-- / pages results box -->
+
+        <!-- menus results box -->
+        <?php echo $search->searchMenus($db, $search->string, $lang); ?>
+        <!-- / menus results box -->
+
+        <!-- menus results box -->
+        <?php echo $search->searchUsers($db, $search->string, $lang); ?>
+        <!-- / menus results box -->
+
+        <!-- widgets results box -->
+        <?php echo $search->searchWidgets($db, $search->string, $lang); ?>
+        <!-- / widgets results box -->
     </div>
     <div class="col-md-4">
         <!-- box -->
         <div class="box">
             <div class="box-header with-border">
-                <h3 class="box-title"><?php echo $lang['SETTINGS'] ?> <small><?php echo $lang['SEARCH_SETTINGS']; ?></small></h3>
+                <h3 class="box-title"><?php echo $lang['FILTER'] ?> <small><?php echo $lang['LIMIT_SEARCH']; ?></small></h3>
             </div>
             <div class="box-body">
-                ...
+                    <div class="row">
+                        <div class="col-md-6">
+                            <input id="pages" name="pages" type="checkbox" checked>
+                            <label for="pages"><?php echo $lang['PAGES']; ?></label>
+                        </div>
+                        <div class="col-md-6">
+                            <input id="menus" name="menus" type="checkbox" checked>
+                            <label for="menus"><?php echo $lang['MENUS']; ?></label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <input id="users" name="users" type="checkbox" checked>
+                            <label for="users"><?php echo $lang['USERS']; ?></label>
+                        </div>
+                        <div class="col-md-6">
+                            <input id="widgets" name="widgets" type="checkbox" checked>
+                            <label for="widgets"><?php echo $lang['WIDGETS']; ?></label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <input id="blogs" name="blogs" type="checkbox" checked>
+                            <label for="blogs"><?php echo $lang['BLOG']; ?></label>
+                        </div>
+                        <div class="col-md-6">
+                            <input id="files" name="files" type="checkbox" checked>
+                            <label for="files"><?php echo $lang['FILES']; ?></label>
+                        </div>
+                    </div>
+
+                <br>
+                <div class="pull-right">
+                    <button class="btn btn-success" type="submit"><i class="fa fa-refresh"></i> &nbsp;<?php echo $lang['REFRESH']; ?></button>
+                </div>
             </div>
         </div>
     </div>
 </div>
+</form>
---
+++ system/classes/search.php
@@ -0,0 +1,148 @@
+<?php
+namespace YAWK {
+    /**
+     * <b>Backend Search</b>
+     *
+     * handles the backend's core search functions. <br>
+     * See Methods Summary for Details!</i></p>
+     *
+     * @author     Daniel Retzl <danielretzl@gmail.com>
+     * @copyright  2017 Daniel Retzl yawk.io
+     * @license    http://www.gnu.org/licenses/gpl-2.0  GNU/GPL 2.0
+     * @version    1.0.0
+     * @link       http://yawk.io
+     * @annotation Backend search class
+     */
+    class search
+    {
+        /** * @searchString string contains the search term */
+        public $string;
+        /** * @searchResult array contains the search result */
+        public $searchResult;
+
+        /**
+         * search pages and draw box
+         * @param string $searchString
+         * @param object $db database object
+         */
+        public function searchPages($db, $string, $lang)
+        {
+            $i = 0;
+            echo "
+            <div class=\"box\">
+                <div class=\"box-header with-border\">
+                    <h3 class=\"box-title\">$lang[PAGES] <small>$lang[ALL_ELEMENTS]</small></h3>
+                </div>
+            <div class=\"box-body\">";
+            echo "<h4>";
+            if (isset($string) && (!empty($string)))
+            {
+                if ($res = $db->query("SELECT id, alias, title FROM {pages} WHERE alias OR title LIKE '%".$string."%'"))
+                {
+                    while ($row = mysqli_fetch_assoc($res))
+                    {
+                        $i++;
+                        echo "<a href=\"index.php?page=page-edit&alias=$row[alias]&id=$row[id]\" target=\"_self\"><i class=\"fa fa-edit\"></i> $row[alias].html</a><br>";
+                    }
+                }
+            }
+            echo "</h4><b>$i</b> $lang[PAGE_ENTRIES_WITH_TAG] <i><b>&laquo;$string&raquo;</b></i></div>
+                </div>";
+        } /* end function searchPages(); */
+
+
+        /**
+         * search menus
+         * @param string $searchString
+         * @param object $db database object
+         */
+        public function searchMenus($db, $string, $lang)
+        {
+            $i = 0;
+            echo "
+            <div class=\"box\">
+                <div class=\"box-header with-border\">
+                    <h3 class=\"box-title\">$lang[MENUS] <small>$lang[ALL_ELEMENTS]</small></h3>
+                </div>
+            <div class=\"box-body\">";
+            echo "<h4>";
+            if (isset($string) && (!empty($string)))
+            {
+                if ($res = $db->query("SELECT menuID, text FROM {menu} WHERE text LIKE '%".$string."%'"))
+                {
+                    while ($row = mysqli_fetch_assoc($res))
+                    {
+                        $i++;
+                        echo "<a href=\"index.php?page=menu-edit&menu=$row[menuID]\" target=\"_self\"><i class=\"fa fa-edit\"></i> $row[text]</a><br>";
+                    }
+                }
+            }
+            echo "</h4><b>$i</b> $lang[MENU_ENTRIES_WITH_TAG] <i><b>&laquo;$string&raquo;</b></i></div>
+                </div>";
+        } /* end function searchPages(); */
+
+
+        /**
+         * search users
+         * @param string $searchString
+         * @param object $db database object
+         */
+        public function searchUsers($db, $string, $lang)
+        {
+            $i = 0;
+            echo "
+            <div class=\"box\">
+                <div class=\"box-header with-border\">
+                    <h3 class=\"box-title\">$lang[USERS] <small>$lang[ALL_ELEMENTS]</small></h3>
+                </div>
+            <div class=\"box-body\">";
+            echo "<h4>";
+            if (isset($string) && (!empty($string)))
+            {
+                if ($res = $db->query("SELECT id, username FROM {users} WHERE username LIKE '%".$string."%'"))
+                {
+                    while ($row = mysqli_fetch_assoc($res))
+                    {
+                        $i++;
+                        echo "<a href=\"index.php?page=users&user=$row[id]\" target=\"_self\"><i class=\"fa fa-edit\"></i> $row[username]</a><br>";
+                    }
+                }
+            }
+            echo "</h4><b>$i</b> $lang[USER_ENTRIES_WITH_TAG] <i><b>&laquo;$string&raquo;</b></i></div>
+                </div>";
+        } /* end function searchPages(); */
+
+
+        /**
+         * search users
+         * @param string $searchString
+         * @param object $db database object
+         */
+        public function searchWidgets($db, $string, $lang)
+        {
+            $i = 0;
+            echo "
+            <div class=\"box\">
+                <div class=\"box-header with-border\">
+                    <h3 class=\"box-title\">$lang[WIDGETS] <small>$lang[ALL_ELEMENTS]</small></h3>
+                </div>
+            <div class=\"box-body\">";
+            echo "<h4>";
+            if (isset($string) && (!empty($string)))
+            {
+                if ($res = $db->query("SELECT id, widgetTitle FROM {widgets} WHERE widgetTitle LIKE '%".$string."%'"))
+                {
+                    while ($row = mysqli_fetch_assoc($res))
+                    {
+                        $i++;
+                        echo "<a href=\"index.php?page=widget-edit&widget=$row[id]\" target=\"_self\"><i class=\"fa fa-edit\"></i> $row[widgetTitle]</a><br>";
+                    }
+                }
+            }
+            echo "</h4><b>$i</b> $lang[WIDGETS_ENTRIES_WITH_TAG] <i><b>&laquo;$string&raquo;</b></i></div>
+                </div>";
+        } /* end function searchPages(); */
+
+
+    } /* end class Search */
+}
