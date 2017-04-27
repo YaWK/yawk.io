a668c3b42b7a491a5b960ddf0471955c39136af0
Daniel Retzl <danielretzl@gmail.com>
2017-04-27 03:03:33 +0200

    updated widget-edit view

--- admin/includes/widget-edit.php
+++ admin/includes/widget-edit.php
@@ -85,11 +85,11 @@ echo "
     <!-- Content Header (Page header) -->
     <section class=\"content-header\">";
 /* draw Title on top */
-echo \YAWK\backend::getTitle($widget->name, $lang['WIDGET_EDIT_SUBTEXT']);
+echo \YAWK\backend::getTitle($widget->name, $lang['WIDGET']);
 echo"<ol class=\"breadcrumb\">
             <li><a href=\"index.php\" title=\"$lang[DASHBOARD]\"><i class=\"fa fa-dashboard\"></i> $lang[DASHBOARD]</a></li>
             <li><a href=\"index.php?page=widgets\" title=\"$lang[WIDGETS]\"> $lang[WIDGETS]</a></li>
-            <li class=\"active\"><a href=\"index.php?page=widget-edit&widget=$widget->id\" title=\"$lang[WIDGET_EDIT_SUBTEXT]\"> $lang[WIDGET_EDIT_SUBTEXT]</a></li>
+            <li class=\"active\"><a href=\"index.php?page=widget-edit&widget=$widget->id\" title=\"$lang[WIDGET_EDIT_SUBTEXT]\"> $lang[EDIT] $widget->name $lang[WIDGET]</a></li>
          </ol>
     </section>
     <!-- Main content -->
@@ -116,25 +116,23 @@ echo"<ol class=\"breadcrumb\">
 </script>
 <!-- FORM -->
 <form name="form" role="form" action="index.php?page=widget-edit&widget=<?php echo $widget->id; ?>" method="post">
-
+<div class="row">
 <!-- LEFT -->
 <div class="col-md-6">
 <!-- BASIC WIDGET SETTINGS -->
 <div class="box box-default">
   <div class="box-header with-border">
-    <h3 class="box-title"><?php echo $widget->name; echo $lang['WIDGET']; ?></h3>
+    <h3 class="box-title"><?php echo "$lang[COMMON_WIDGET_SETTINGS]"; ?></h3>
   </div>
   <div class="box-body">
     <!-- WIDGET -->
-    <label for="widgetType"><?php echo $lang['WIDGET']; ?>&nbsp;
+    <label for="widgetType"><?php echo $lang['WIDGET']; ?></label>
      <select id="widgetType" name="widgetType" class="form-control">
      <option value="<?php echo $widget->widgetType; ?>"><?php echo $widget->name; ?></option>
      </select>
-    </label>
-    <br>
     <!-- PAGE -->
-    <label for="pageID"><?php echo $lang['ON_PAGE']; ?> &nbsp;
-        <select id="pageID" name="pageID" class="form-control">    
+    <label for="pageID"><?php echo $lang['ON_PAGE']; ?></label>
+        <select id="pageID" name="pageID" class="form-control">
         <option value="<?php echo $widget->getWidgetId($db, $widget->id); ?>"><?php echo $widget->getWidget($db, $widget->id); ?></option>
         <option value="0"><?php echo $lang['ON_ALL_PAGES']; ?></option>
         <?php
@@ -144,59 +142,50 @@ echo"<ol class=\"breadcrumb\">
             }
         ?>
       </select>
-    </label>
-    <br>
   <!-- POSITION -->
-  <label for="positions"><?php echo $lang['AT_POSITION']; ?> &nbsp;
+  <label for="positions"><?php echo $lang['AT_POSITION']; ?></label>
     <select id="positions" name="positions" class="form-control">
      <option value="<?php echo $widget->position; ?>"><?php echo $widget->position; ?></option>
      <option value="">-----</option>
 
     <?php /* get tpl positions */
       $i = 0;
+      $position = array();
       foreach(\YAWK\template::getTemplatePositions($db) as $position[]){
         echo "<option value=\"".$position[$i]."\">".$position[$i]."</option>";
         $i++;
       }
     ?>
    </select>
-  </label>
 
   <br>
   <!-- DATE_PUBLISH -->
-  <label for ="date_publish"><?php echo $lang['START_PUBLISH']; ?> &nbsp;
+  <label for ="datetimepicker1"><?php echo $lang['START_PUBLISH']; ?></label>
       <input id="datetimepicker1" name="date_publish" class="form-control" value="<?php echo $widget->date_publish; ?>">
-  </label>
-  <br>
+
   <!-- DATE_UNPUBLISH -->
-  <label for ="date_unpublish"><?php echo $lang['END_PUBLISH']; ?> &nbsp;
+  <label for ="datetimepicker2"><?php echo $lang['END_PUBLISH']; ?></label>
     <input id="datetimepicker2" name="date_unpublish" class="form-control" value="<?php echo $widget->date_unpublish; ?>">
-  </label>
-  <br><br>
+  <br>
       <!-- MARGIN TOP -->
-      <label><?php echo "$lang[MARGIN_TOP] <i><small>$lang[LEAVE_BLANK_FOR_NO_MARGIN]</small></i>"; ?> &nbsp;
+      <label><?php echo "$lang[MARGIN_TOP] <i><small>$lang[LEAVE_BLANK_FOR_NO_MARGIN]</small></i>"; ?></label>
           <input type="text" class="form-control" placeholder="" name="marginTop" id="marginTop" maxlength="11" value="<?php echo $widget->marginTop; ?>">
-      </label><br>
       <!-- MARGIN BOTTOM -->
-      <label><?php echo "$lang[MARGIN_BOTTOM] <i><small>$lang[LEAVE_BLANK_FOR_NO_MARGIN]</small></i>"; ?> &nbsp;
+      <label><?php echo "$lang[MARGIN_BOTTOM] <i><small>$lang[LEAVE_BLANK_FOR_NO_MARGIN]</small></i>"; ?></label>
           <input type="text" class="form-control" name="marginBottom" placeholder="" id="marginBottom" maxlength="11" value="<?php echo $widget->marginBottom; ?>">
-      </label><br><br>
-  <!-- SORT -->
-  <label><?php echo $lang['SORTATION']; ?> &nbsp;
-  <input type="text" class="form-control" name="sort" maxlength="6" value="<?php echo $widget->sort; ?>">
-  </label><br>
+      <br>
+  <!-- SORTATION -->
+  <label for="sort"><?php echo $lang['SORTATION_ORDER']; ?></label>
+  <input id="sort" type="text" class="form-control" name="sort" maxlength="6" value="<?php echo $widget->sort; ?>">
 
-      <!-- TITLE -->
-      <label for ="widgetTitle"><?php echo $lang['DESCRIPTION']; ?> &nbsp;
-          <input id="widgetTitle" name="widgetTitle" class="form-control" value="<?php echo $widget->widgetTitle; ?>">
-      </label>
-      <br><br>
+  <!-- TITLE -->
+  <label for ="widgetTitle"><?php echo $lang['DESCRIPTION']; ?></label>
+  <input id="widgetTitle" name="widgetTitle" class="form-control" value="<?php echo $widget->widgetTitle; ?>">
+  <br>
 
+  <!-- PUBLISHED / UNPUBLISHED CHECKBOX -->
   <?php if ($widget->published == "1") { $checkedHtml="checked=\"checked\""; } else $checkedHtml = ''; ?>
-
-  <label><?php echo "$lang[PUBLISHED]"; ?> &nbsp;<input name="publish" value="1" type="checkbox" <?php echo $checkedHtml ?>>&nbsp;
-  </label>
-
+  <input id="publish" name="publish" value="1" type="checkbox" <?php echo $checkedHtml ?>> <label for="publish"><?php echo "$lang[PUBLISHED]"; ?></label>
 
   </div>
 </div>
@@ -206,39 +195,23 @@ echo"<ol class=\"breadcrumb\">
   <div class="col-md-6">
     <div class="box box-default">
     <div class="box-header with-border">
-    <h3 class="box-title"><?php echo "$lang[EXTENDED] $lang[WIDGET] $lang[SETTINGS]"; ?></h3>
+    <h3 class="box-title"><?php echo "$widget->name $lang[WIDGET] $lang[SETTINGS]"; ?></h3>
+
+        <button type="submit" id="savebutton" name="save" class="btn btn-success pull-right">
+            <i id="savebuttonIcon" class="fa fa-check"></i> &nbsp;<?php echo $lang['WIDGETS_SAVE_BTN']; ?>
+        </button>
   </div>
   <div class="box-body">
   <!-- MORE WIDGET SETTINGS -->
   <?php
-
-  $widget->getWidgetFormElements($db, "", $widget->id, $lang);
-  /*
-    if ($res = $db->query("SELECT cws.property, cws.value, cwd.fieldClass, cwd.description, cw.widgetType as widget
-    FROM {widgets} as cw
-    JOIN {widget_settings} as cws ON cws.widgetID = cw.id
-    JOIN {widget_defaults} as cwd ON cwd.widgetType = cws.widgetType AND cwd.property = cws.property
-    WHERE cws.activated = 1 && cw.id = '".$widget->id."'"))
-    {
-        while($row = mysqli_fetch_assoc($res))
-        {
-            echo "<label for=\"".htmlentities($row['property'])."\">";
-            echo $row['description'].":";
-            echo "</label>";
-            echo "<input type=\"text\" class=\""; echo $row['fieldClass']; echo"\" name=\"".htmlentities($row['property'])."\" value=\"".htmlentities($row['value'])."\">";
-        }
-    }
-*/
-?>
+    // draw settings (form elements) for this widget
+    $widget->getWidgetFormElements($db, "", $widget->id, $lang);
+  ?>
   <br><input type="hidden" name="widgetID" value="<?php echo $widget->id; ?>">
   <br>
 
-      <button type="submit" id="savebutton" name="save" class="btn btn-success">
-          <i id="savebuttonIcon" class="fa fa-check"></i> &nbsp;<?php echo $lang['WIDGETS_SAVE_BTN']; ?>
-      </button>
+  </div>
+    </div>
   </div>
 </div>
-
-</div>
-
 </form>
