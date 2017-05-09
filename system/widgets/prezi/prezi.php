c73b9868b28e4c8cfac39efd11cfa2f317f66785
Daniel Retzl <danielretzl@gmail.com>
2017-05-10 01:27:02 +0200

    updated prezi widget (added heading and subtitle)

--- system/widgets/prezi/prezi.php
+++ system/widgets/prezi/prezi.php
@@ -24,12 +24,40 @@ if (isset($_GET['widgetID']))
 				case 'preziHtml';
 					$preziHtml = $w_value;
 					break;
+				case 'preziHeading';
+					$heading = $w_value;
+					break;
+				case 'preziSubtext';
+					$subtext = $w_value;
+					break;
 			}
 		} /* END LOAD PROPERTIES */
 
 	} // end while fetch row (fetch widget settings)
 } // if no widget ID is given or settings could not be retrieved, use this as defaults:
 
+// if a heading is set and not empty
+if (isset($heading) && (!empty($heading)))
+{   // add a h1 tag to heading string
+	$heading = "$heading";
+
+	// if subtext is set, add <small> subtext to string
+	if (isset($subtext) && (!empty($subtext)))
+	{   // build a headline with heading and subtext
+		$subtext = "<small>$subtext</small>";
+		$headline = "<h1>$heading&nbsp;"."$subtext</h1>";
+	}
+	else
+	{   // build just a headline - without subtext
+		$headline = "<h1>$heading</h1>";    // draw just the heading
+	}
+}
+else
+{   // leave empty if it's not set
+	$headline = '';
+}
+
 // output prezi html
+echo $headline;
 echo $preziHtml;
 ?>
