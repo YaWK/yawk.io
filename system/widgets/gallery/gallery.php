972937a2a40fbcdd186e42c527065e0958e74188
Daniel Retzl <danielretzl@gmail.com>
2017-05-10 03:53:23 +0200

    updated gallery plugin (added heading)

--- system/widgets/gallery/gallery.php
+++ system/widgets/gallery/gallery.php
@@ -9,20 +9,59 @@
 </script>
 
 <?php
+$galleryID = 1;
+$heading = '';
+$subtext = '';
+
 // $_GET['widgetID'] will be generated in \YAWK\widget\loadWidgets($db, $position)
-if (isset($_GET['widgetID']))
-{
+if (isset($_GET['widgetID'])) {
     $widgetID = $_GET['widgetID'];
-    $res = $db->query("SELECT value FROM {widget_settings}
-								WHERE widgetID = '".$widgetID."'
+    $res = $db->query("SELECT * FROM {widget_settings}
+								WHERE widgetID = '" . $widgetID . "'
 	                        	AND activated = '1'");
-    $row = mysqli_fetch_array($res);
-    $galleryID = $row[0];
-}
-else
-    {
-        $galleryID = 0;
+    while ($row = mysqli_fetch_assoc($res)) {   // set widget properties and values into vars
+        $w_property = $row['property'];
+        $w_value = $row['value'];
+        $w_widgetType = $row['widgetType'];
+        $w_activated = $row['activated'];
+        /* end of get widget properties */
+
+        /* filter and load those widget properties */
+        if (isset($w_property)) {
+            switch ($w_property) {
+                /* url of the stream */
+                case 'galleryID';
+                    $galleryID = $w_value;
+                    break;
+
+                /* heading */
+                case 'galleryHeading';
+                    $heading = $w_value;
+                    break;
+
+                /* subtext */
+                case 'gallerySubtext';
+                    $subtext = $w_value;
+                    break;
+            }
+        } /* END LOAD PROPERTIES */
+    } // end while fetch row (fetch widget settings)
+
+// if a heading is set and not empty
+    if (isset($heading) && (!empty($heading))) {   // add a h1 tag to heading string
+        $heading = "$heading";
+
+        // if subtext is set, add <small> subtext to string
+        if (isset($subtext) && (!empty($subtext))) {   // build a headline with heading and subtext
+            $subtext = "<small>$subtext</small>";
+            $headline = "<h1>$heading&nbsp;" . "$subtext</h1>";
+        } else {   // build just a headline - without subtext
+            $headline = "<h1>$heading</h1>";    // draw just the heading
+        }
+    } else {   // leave empty if it's not set
+        $headline = '';
     }
+}
 ?>
 
 <?php
@@ -37,4 +76,5 @@ if (!isset($db))
     require_once 'system/classes/db.php';
     $db = new \YAWK\db();
 }
+echo $headline;
 $gallery->drawImageGallery($db, $galleryID);
