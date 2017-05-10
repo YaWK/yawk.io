70b18bfb9268a46add24fd43041be6d697928134
Daniel Retzl <danielretzl@gmail.com>
2017-05-10 21:24:18 +0200

    issue #70 updated loginbox widget and language files

--- system/widgets/loginbox/loginbox.php
+++ system/widgets/loginbox/loginbox.php
@@ -17,9 +17,10 @@ $user = new YAWK\user();
   if(isset($_POST['user']))
   { // if username is set
   	 // call login method 
-    if($user->login($db, $_POST['user'],$_POST['password'])) {
+    if($user->login($db, $_POST['user'],$_POST['password']))
+    {
 		 if(isset($_SESSION['username']) && $_SESSION['logged_in'] == true)
-		 {	// user is logged in, redirect to welcome controller
+		 {	// user is logged in, redirect to welcome page
 			 \YAWK\sys::setTimeout("welcome.html",0);
 			 exit;
 		 }
@@ -50,34 +51,63 @@ if(isset($_SESSION['username']) && $_SESSION['logged_in'] == true)
 		<a href=\"".$currentWidgetPath."".$logoutFile."\" class=\"btn btn-danger active\" target_\"self\">logout</a></form>";
 }
 
-else { 
-// get widget settings and draw login box
-if (isset($wID))
-{		$w_value = "";
-		/* get widget settings */    
-		/* ESSENTIAL TO GET WIDGETS WORK PROPERLY */
-	    $res = $db->query("SELECT * FROM {widget_settings}
-	                        WHERE widgetID = '".$wID."'
-	                        AND activated = '1'");
-	    while($row = mysqli_fetch_row($res)) {
-	      $w_property = $row[1];   
-	      $w_value = $row[2];
-	      $w_widgetType = $row[3];
-	      $w_activated = $row[4];
-	      }
-}
+else
+    {   // user is NOT logged in
+        // get widget settings and draw login box
+    // $_GET['widgetID'] will be generated in \YAWK\widget\loadWidgets($db, $position)
+    if (isset($_GET['widgetID']))
+    {
+        // widget ID
+        $widgetID = $_GET['widgetID'];
 
-		/* LOAD PROPERTY LOGINBUTTON (text) */
-		if (isset($w_property)){
-			if ($w_property === "buttontitle") {
-				$buttoncode = 	$w_value;	
-			} 
-			else { 
-				$buttoncode="Login"; }
-		}
-		else { $buttoncode="Login"; } // default setting for content include
-		/* end load property loginbuttontext */
-	 
-	// draw login box
+        // get widget settings from db
+        $res = $db->query("SELECT * FROM {widget_settings}
+	                        WHERE widgetID = '".$widgetID."'
+	                        AND activated = '1'");
+        while($row = mysqli_fetch_assoc($res))
+        {   // set widget properties and values into vars
+            $w_property = $row['property'];
+            $w_value = $row['value'];
+            $w_widgetType = $row['widgetType'];
+            $w_activated = $row['activated'];
+            /* end of get widget properties */
+
+            /* filter and load those widget properties */
+            if (isset($w_property)){
+                switch($w_property)
+                {
+                    /* url of the video to stream */
+                    case 'loginboxButtonText';
+                        $buttonText = $w_value;
+                        break;
+                }
+            } /* END LOAD PROPERTIES */
+        } // end while fetch row (fetch widget settings)
+    }
+
+    // if a heading is set and not empty
+    if (isset($heading) && (!empty($heading)))
+    {   // add a h1 tag to heading string
+        $heading = "$heading";
+
+        // if subtext is set, add <small> subtext to string
+        if (isset($subtext) && (!empty($subtext)))
+        {   // build a headline with heading and subtext
+            $subtext = "<small>$subtext</small>";
+            $headline = "<h1>$heading&nbsp;"."$subtext</h1>";
+        }
+        else
+        {   // build just a headline - without subtext
+            $headline = "<h1>$heading</h1>";    // draw just the heading
+        }
+    }
+    else
+    {   // leave empty if it's not set
+        $headline = '';
+    }
+
+    // draw headline, if set
+    echo $headline;
+    // draw login box
     echo \YAWK\user::drawLoginBox("","");
-	  }
+}
