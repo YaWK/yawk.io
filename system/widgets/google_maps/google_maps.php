a52c305461ac4e3dad81a870df5377b0e7e02ced
Daniel Retzl <danielretzl@gmail.com>
2017-05-10 20:24:20 +0200

    issue #70 updated googlemaps widget and added language files

--- system/widgets/google_maps/google_maps.php
+++ system/widgets/google_maps/google_maps.php
@@ -32,6 +32,29 @@ if (isset($_GET['widgetID']))
 	}
 }
 
+// if a heading is set and not empty
+if (isset($heading) && (!empty($heading)))
+{   // add a h1 tag to heading string
+	$heading = "$heading";
+
+	// if subtext is set, add <small> subtext to string
+	if (isset($subtext) && (!empty($subtext)))
+	{   // build a headline with heading and subtext
+		$subtext = "<small>$subtext</small>";
+		$headline = "<h1>$heading&nbsp;"."$subtext</h1>";
+	}
+	else
+	{   // build just a headline - without subtext
+		$headline = "<h1>$heading</h1>";    // draw just the heading
+	}
+}
+else
+{   // leave empty if it's not set
+	$headline = '';
+}
+// display headline, if its set
+echo $headline;
+
 // GoogleMap: onScreen!
 echo $googleMapsEmbedHtmlCode;
 ?>
