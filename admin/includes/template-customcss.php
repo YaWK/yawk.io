2472c3e35a50238e62b6f64f202615f33fadc9dc
Daniel Retzl <danielretzl@gmail.com>
2017-09-06 18:12:58 +0200

    issue #92: basic implementation of custom css with editor

--- admin/includes/template-customcss.php
+++ admin/includes/template-customcss.php
@@ -1,3 +1,4 @@
+
 <?php
 // get settings for editor
 $editorSettings = \YAWK\settings::getEditorSettings($db, 14);
@@ -127,3 +128,44 @@ $editorSettings = \YAWK\settings::getEditorSettings($db, 14);
 
     }); // end document ready
 </script>
+<?php
+// new template object if not exists
+if (!isset($template)) { $template = new \YAWK\template(); }
+// new user object if not exists
+if (!isset($user)) { $user = new \YAWK\user(); }
+// $_GET['id'] or $_POST['id'] holds the template ID to edit.
+// If any one of these two is set, we're in "preview mode" - this means:
+// The user database holds two extra cols: overrideTemplate(int|0,1) and templateID
+// Any user who is allowed to override the Template, can edit a template and view it
+// in the frontend. -Without affecting the current active theme for any other user.
+// This is pretty cool when working on a new design: because you see changes, while others wont.
+// In theory, thereby every user can have a different frontend template activated.
+?>
+
+<!-- CUSTOM CSS -->
+<h3>Custom.css <small><?php echo $lang['TPL_CUSTOMCSS_SUBTEXT']; ?></small></h3>
+    <div class="row">
+        <div class="col-md-8">
+            <?php $customCSS = $template->getCustomCSSFile($db, $template->id); ?>
+            <textarea name="customCSS" cols="64" rows="28" id="summernote"><?php echo $customCSS; ?></textarea>
+            <label for="summernote"><small><?php echo $lang['YOU_EDIT']; ?>:</small> &nbsp;system/templates/<?php echo $template->name; ?>/css/custom.css</label>
+        </div>
+        <div class="col-md-4">
+            <div class="box box-default">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><?php echo $template->name; ?>/css/custom.css</h3>
+                </div>
+                <div class="box-body">
+                    <?php echo $lang['CUSTOM_CSS_DESC']; ?>
+                    <br><br>
+                    <i><?php echo $lang['CUSTOM_CSS_HELP']; ?></i><br>
+                    &raquo; <a href="http://www.w3schools.com/css/" title="open CSS overview in new TAB" target="_blank">w3schools.com/css/</a>
+
+                    <hr>
+                    <b><i class="fa fa-lightbulb-o"></i> <?php echo $lang['DID_YOU_KNOW']; ?></b><br>
+                    <i><?php echo $lang['TIP_STRG_S']; ?></i>
+                </div>
+
+            </div>
+        </div>
+    </div>
