3cbea0f517f4631dfa71ae142b2ae9c50c0a2773
Daniel Retzl <danielretzl@gmail.com>
2017-09-10 22:28:11 +0200

    Update index.php
    disabled error reporting

--- index.php
+++ index.php
@@ -30,9 +30,9 @@
  */
 session_start();
 /* Error Reporting - this is for DEVELOPMENT PURPOSE ONLY! */
-error_reporting(E_ALL ^ E_STRICT);
-ini_set('display_errors', 1);
-error_reporting(1);
+// error_reporting(E_ALL ^ E_STRICT);
+ini_set('display_errors', 0);
+error_reporting(0);
 /* include core files */
 require_once('system/classes/db.php');               // database connection
 require_once('system/classes/settings.php');         // get/set settings from settings db
