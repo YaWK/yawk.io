726c14f0f86a82856939f09498d2711d29dfd0b2
Daniel Retzl <danielretzl@gmail.com>
2017-09-25 21:22:58 +0200

    issue #109: added block adblock widget

---
+++ system/widgets/fuckadblock/fuckadblock.php
@@ -0,0 +1,125 @@
+<!-- fuckAdBlock -->
+<script src="system/engines/fuckAdBlock/fuckAdBlock.js"></script>
+<script type="text/javascript">
+    $( document ).ready(function() {
+        // Function called if AdBlock is not detected
+        function adBlockNotDetected() {
+            // alert('AdBlock is not enabled');
+        }
+        // Function called if AdBlock is detected
+        function adBlockDetected() {
+            // alert('AdBlock is enabled');
+            // $('#myModal').modal('show');
+            $('#myModal').modal({backdrop: 'static', keyboard: false})
+        }
+        // Recommended audit because AdBlock lock the file 'fuckadblock.js'
+        // If the file is not called, the variable does not exist 'fuckAdBlock'
+        // This means that AdBlock is present
+        if(typeof fuckAdBlock === 'undefined') {
+            adBlockDetected();
+        } else {
+            fuckAdBlock.onDetected(adBlockDetected);
+            fuckAdBlock.onNotDetected(adBlockNotDetected);
+            // and|or
+            fuckAdBlock.on(true, adBlockDetected);
+            fuckAdBlock.on(false, adBlockNotDetected);
+            // and|or
+            fuckAdBlock.on(true, adBlockDetected).onNotDetected(adBlockNotDetected);
+        }
+    });
+</script>
+<?php
+$title = '';
+$text = '';
+$level = 'low';
+$lowLevelBtnText = 'OK, I got it';
+$highLevelBtnText = 'Please disable your AdBlocker and click here.';
+$footerBtnCode = '<button type="button" class="btn btn-danger" data-dismiss="modal">'.$lowLevelBtnText.'</button>';
+$headerBtnCode = '<button type="button" class="close" data-dismiss="modal">&times;</button>';
+$btnClass = 'btn btn-danger';
+
+// $_GET['widgetID'] will be generated in \YAWK\widget\loadWidgets($db, $position)
+if (isset($_GET['widgetID'])) {
+    $widgetID = $_GET['widgetID'];
+    $res = $db->query("SELECT * FROM {widget_settings}
+								WHERE widgetID = '" . $widgetID . "'
+	                        	AND activated = '1'");
+    while ($row = mysqli_fetch_assoc($res)) {   // set widget properties and values into vars
+        $w_property = $row['property'];
+        $w_value = $row['value'];
+        $w_widgetType = $row['widgetType'];
+        $w_activated = $row['activated'];
+        /* end of get widget properties */
+
+        /* filter and load those widget properties */
+        if (isset($w_property)) {
+            switch ($w_property) {
+                /* title */
+                case 'fuckAdBlockTitle';
+                    $title = $w_value;
+                    break;
+
+                /* text */
+                case 'fuckAdBlockText';
+                    $text = $w_value;
+                    break;
+
+                /* level */
+                case 'fuckAdBlockLevel';
+                    $level = $w_value;
+                    break;
+
+                /* lowLevelBtnText */
+                case 'fuckAdBlockLowBtnText';
+                    $lowLevelBtnText = $w_value;
+                    break;
+
+                /* highLevelBtnText */
+                case 'fuckAdBlockHighBtnText';
+                    $highLevelBtnText = $w_value;
+                    break;
+
+                /* highLevelBtnText */
+                case 'fuckAdBlockBtnClass';
+                    $btnClass = $w_value;
+                    break;
+            }
+        } /* END LOAD PROPERTIES */
+    } // end while fetch row (fetch widget settings)
+
+    // check priority levels -
+    // high means that the user needs to disable his adblocker to see the content
+    // low generates a more fair-use user-friendy de-clickable info message box.
+    if ($level === "high")
+    {
+        $link = $_SERVER['REQUEST_URI'];
+        $footerBtnCode = '<a href="'.$link.'" class="'.$btnClass.'" style="color:#fff; text-shadow: none;">'.$highLevelBtnText.'</a>';
+        $headerBtnCode = '';
+    }
+    if ($level === "low")
+    {
+        $footerBtnCode = '<button type="button" class="'.$btnClass.'" data-dismiss="modal">'.$lowLevelBtnText.'</button>';
+        $headerBtnCode = '<button type="button" class="close" data-dismiss="modal">&times;</button>';
+    }
+}
+?>
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <?php echo $headerBtnCode; ?>
+                <h4><?php echo $title; ?></h4>
+            </div>
+            <div class="modal-body">
+                <?php echo $text; ?>
+            </div>
+            <div class="modal-footer">
+                <?php echo $footerBtnCode; ?>
+            </div>
+        </div>
+
+    </div>
+</div>
