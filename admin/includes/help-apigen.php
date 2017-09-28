1b1eea59c4529b635e65adabd39f2071905ca2da
Daniel Retzl <danielretzl@gmail.com>
2017-09-28 16:45:15 +0200

    issue #116: add / update help pages

--- admin/includes/help-apigen.php
+++ admin/includes/help-apigen.php
@@ -1,3 +1,15 @@
+<?php 
+  YAWK\backend::getTitle($lang['HELP'], $lang['HELP_SUBTEXT']);
+?>
+<style type="text/css">
+    iframe:focus {
+        outline: none;
+    }
+
+    iframe[seamless] {
+        display: block;
+    }
+</style>
 <?php
 // TEMPLATE WRAPPER - HEADER & breadcrumbs
 echo "
@@ -20,20 +32,18 @@ echo"<ol class=\"breadcrumb\">
 <a class="btn btn-success pull-right" href="index.php?page=syslog&clear=1">
     <i class="fa fa-trash-o"></i> &nbsp;<?php print $lang['HELP']; ?></a>
 
-<div class="row">
-<div class="col-md-6">
-    <div class="box box-default">
-        <div class="box-header with-border">
-            <h3 class="box-title"><?php echo $lang['HELP']; ?></h3>
-        </div>
-        <div class="box-body">
-            help body
-        </div>
+<div class="box box-default">
+    <div class="box-header with-border">
+        <h3 class="box-title"><?php echo $lang['HELP']; ?></h3>
+    </div>
+    <div class="box-body">
+<!-- APIGEN IFRAME -->
+<iframe src="http://docs.yawk.io/"
+        width="100%"
+        height="840"
+        scrolling="yes"
+        frameborder="0">
+</iframe>
     </div>
 </div>
-<div class="col-md-6">
-    ...
-</div>
-</div>
-
 <br><br><br><br>
