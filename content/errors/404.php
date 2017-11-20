2926f4fbce440a7f1001a3521d76cd11fe392ed2
Daniel Retzl <danielretzl@gmail.com>
2017-11-20 20:01:15 +0100

    updated 404 error page; added line breaks

--- content/errors/404.php
+++ content/errors/404.php
@@ -1,6 +1,15 @@
 <?php
 @header("HTTP/1.0 404 Not Found");
 ?>
+<?php
+// TEMPLATE WRAPPER - HEADER & breadcrumbs
+echo "
+    <!-- Content Wrapper. Contains page content -->
+    <div class=\"content-wrapper\" id=\"content-FX\">
+    <!-- Main content -->
+    <section class=\"content\">";
+/* page content start here */
+?>
 <head>
     <title>404 Fehler - Datei nicht gefunden</title>
     <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
@@ -13,3 +22,4 @@
 		Entweder sie wurde noch nicht erstellt oder verschoben.</h3>
 		<h6><a href="index.php">#gohome</a> </h6>
 	</div>
+<br><br><br><br>
