fbbada4e1aae1e534418a36993ef83c46d36155f
Daniel Retzl <danielretzl@gmail.com>
2017-12-16 04:40:49 +0100

    issue #145: added assets overview

--- admin/includes/template-overview.php
+++ admin/includes/template-overview.php
@@ -235,25 +235,7 @@ echo"</section><!-- Main content -->
                 <dt><?php echo $lang['TOOLS']; ?></dt>
                 <dd>
                     <b><?php echo $lang['YAWK_SLOGAN_TOGETHER']; ?><br>
-                        <i class="fa fa-check text-light-blue"></i> YaWK 16.9 <small>
-                        <a href="http://www.yawk.io/" target="_blank" title="Official YaWK Website [in new tab]">
-                        <i class="fa fa-external-link"></i></a></small><br>
-
-                        <i class="fa fa-check text-light-blue"></i> Boostrap 3.3.7<small>
-                        <a href="http://www.getbootstrap.com/" target="_blank" title="Official Bootstrap Website [in new tab]">
-                        <i class="fa fa-external-link"></i></a></small><br>
-
-                        <i class="fa fa-check text-light-blue"></i> jQuery 1.11.3<small>
-                        <a href="http://www.jquery.com/" target="_blank" title="Official jQuery Website [in new tab]">
-                        <i class="fa fa-external-link"></i></a></small><br>
-
-                        <i class="fa fa-check text-light-blue"></i> FontAwesome 4.6.3<small>
-                        <a href="http://www.fontawesome.io/" target="_blank" title="Official FontAwesome Website [in new tab]">
-                        <i class="fa fa-external-link"></i></a></small><br>
-
-                        <i class="fa fa-check text-light-blue"></i> Animate CSS 3.5.2<small>
-                        <a href="http://www.fontawesome.io/" target="_blank" title="Official FontAwesome Website [in new tab]">
-                        <i class="fa fa-external-link"></i></a></small><br>
+                        <?php \YAWK\template::drawAssetsTitles($db, $template->id); ?>
                 </dd>
                 <dt>&nbsp;</dt>
                 <dd>&nbsp;</dd>
