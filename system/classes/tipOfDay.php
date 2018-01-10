a510d59250f2e0d9bfb731f075c57d8aa7f9b3cf
Daniel Retzl <danielretzl@gmail.com>
2018-01-11 00:54:23 +0100

    issue #157: added tip of day class

---
+++ system/classes/tipOfDay.php
@@ -0,0 +1,216 @@
+<?php
+namespace YAWK {
+    /**
+     * <b>Display the tip of the day. <i>(if enabled)</i></b>
+     *
+     * This class handles all tip of the day functions.
+     * Tips are stored in cms_tips database.
+     * <i>Example:</i>
+     * <code><?php YAWK\tipOfDay::displayRandom($db); ?></code>
+     * To show a random tip of the day.
+     * <p><i>Class covers backend functionality.
+     * See Methods Summary for Details!</i></p>
+     *
+     * @author     Daniel Retzl <danielretzl@gmail.com>
+     * @copyright  2009-2015 Daniel Retzl yawk.io
+     * @license    http://www.gnu.org/licenses/gpl-2.0  GNU/GPL 2.0
+     * @version    1.0.0
+     * @link       http://yawk.io
+     * @annotation TipOfDay class serve functions to get and draw a tip of the day
+     */
+    class tipOfDay extends \YAWK\alert
+    {
+        public $id = 1;
+        public $published = 1;
+        public $sortation = 0;
+        public $tipClass = "";
+        public $tipHeading = "";
+        public $tipText = "";
+        public $tipLink = "";
+
+        /**
+         * Draw a tip of the day
+         * @return bool
+         */
+        public function setPublished($db, $id, $published)
+        {
+            /** @var $db \YAWK\db */
+            // update database
+            if ($db->query("UPDATE {tips} SET published = '".$published."' WHERE id = '".$id."'"))
+            {   // all good
+                return true;
+            }
+            else
+            {   // failed to set publish status
+                \YAWK\sys::setSyslog($db, 1, "Failed to set tip of the day ID #$this->id to published state 0",0, 0, 0, 0);
+                return false;
+            }
+        }
+
+        /**
+         * Store a new tip to database
+         * @param object $db Database Object
+         * @param int $published 1 = Tip is published (unseen) 0 = Tip is not published (already seen)
+         * @param string $tipClass css class for the alert box (success, warning, danger or info)
+         * @param string $tipHeading Heading of the tip, up to 255 chars
+         * @param string $tipText Text of the tip, up to 255 chars
+         * @param string $tipLink Link of the tip, up to 255 chars
+         *
+         */
+        public function setTip($db, $published, $sortation, $tipClass, $tipHeading, $tipText, $tipLink)
+        {
+            /** @var $db \YAWK\db */
+            // preprate data
+            if (isset($published) && (!empty($published)))
+            { $db->quote($published); }
+            if (isset($tipClass) && (!empty($tipClass)))
+            { $db->quote($tipClass); }
+            if (isset($sortation) && (!empty($sortation)))
+            { $db->quote($sortation); }
+            if (isset($tipHeading) && (!empty($tipHeading)))
+            { $db->quote($tipHeading); }
+            if (isset($tipText) && (!empty($tipText)))
+            { $db->quote($tipText); }
+            if (isset($tipLink) && (!empty($tipLink)))
+            { $db->quote($tipLink); }
+            // insert tip into database
+            if ($db->query("INSERT INTO {tips} (published, sortation, tipClass, tipHeading, tipText, tipLink)
+                                   VALUES 
+                                   ('".$published."',
+                                    '".$sortation."',
+                                    '".$tipClass."',
+                                    '".$tipHeading."',
+                                    '".$tipText."',
+                                    '".$tipLink."'"))
+            {   // all good
+                return true;
+            }
+            else
+                {   // failed to insert tip into database
+                    return false;
+                }
+        }
+
+        /**
+         * Get a random tip from database that is still unseen.
+         * @param $db
+         * return bool
+         */
+        public function getRandomTipData($db)
+        {
+            /** @var $db \YAWK\db */
+            if (\YAWK\settings::getSetting($db, "backendTipOfDayRepeat") == true)
+            {
+                $sql = "";
+            }
+            else
+            {
+                $sql = "WHERE published = 1";
+            }
+
+            if ($res = $db->query("SELECT * FROM {tips} $sql ORDER BY rand() LIMIT 1"))
+            {
+                // get tip data into array
+                $row = mysqli_fetch_assoc($res);
+                // check data before we return it
+                if (is_array($row) && (!empty($row)))
+                {   // walk through array
+                    foreach ($row as $property => $value)
+                    {   // set current object properties
+                        $this->$property = $value;
+                    }
+                    // all done
+                    return true;
+                }
+                else
+                    {   // array is not set or empty
+                        return false;
+                    }
+            }
+            else
+                {
+                    // query failed
+                    return false;
+                }
+        }
+
+        /**
+         * Get a the next single tip which is not already seen (still published)
+         * To get the order, ID and published fields will be used in the query.
+         * @param $db
+         * return bool
+         */
+        public function getNextTipData($db)
+        {
+            /** @var $db \YAWK\db */
+            // query data: ordered ascending by ID, only entries that are unseen
+            if ($res = $db->query("SELECT * FROM {tips} WHERE published = 1 ORDER BY id ASC LIMIT 1"))
+            {
+                // get tip data into array
+                $row = mysqli_fetch_assoc($res);
+                // check data before we return it
+                if (is_array($row) && (!empty($row)))
+                {   // all good, return data
+                    foreach ($row as $property => $value)
+                    {   // set object properties
+                        $this->$property = $value;
+                    }
+                    // data set, all good -
+                    return true;
+                }
+                else
+                {
+                    // array empty - check if repeat tips is enabled
+                    if (\YAWK\settings::getSetting($db, "backendTipOfDayRepeat") == true)
+                    {
+                        if ($db->query("UPDATE {tips} SET published = 1"))
+                        {
+                            // call function again to show first tip
+                            $this->getNextTipData($db);
+                        }
+                    }
+                    // start again with 1st tip
+                    return true;
+                }
+            }
+            else
+            {   // query failed
+                return false;
+            }
+        }
+
+        /**
+         * Draw tip of the day.
+         * @return string
+        */
+        public function drawTip($db)
+        {
+            if (isset($this->tipLink) && (!empty($this->tipLink)))
+            {
+                if (filter_var($this->tipLink, FILTER_VALIDATE_URL))
+                {
+                    $this->tipText .= "<br><a href=\"#\">$this->tipLink</a>";
+                }
+            }
+            if (isset($this->tipHeading) && (empty($this->tipHeading)))
+            {
+                // todo: last message that tutorial is over?
+                return false;
+            }
+            else if (isset($this->tipText) && (empty($this->tipText)))
+            {
+                // todo: last message that tutorial is over?
+                return false;
+            }
+            else
+                {
+                    if($this->setPublished($db, $this->id, 0) == false)
+                    {
+                        return "failed to set tip to zero";
+                    }
+                    return alert::draw("tipofday", "Tip #$this->id - $this->tipHeading", "$this->tipText", '', 10000);
+                }
+        }
+
+    } /* end class TipOfDay */
+} /* end namespace */
