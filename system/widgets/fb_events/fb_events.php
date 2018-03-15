77cc522cff01b9abeab1b67a657b0d417c1e85a5
Daniel Retzl <danielretzl@gmail.com>
2018-03-15 21:15:53 +0100

    issue #170: updated facebook event widget

--- system/widgets/fb_events/fb_events.php
+++ system/widgets/fb_events/fb_events.php
@@ -1,16 +1,9 @@
 <?php
-$fbEventsAppId = '';
-$fbEventsPageId = '';
-$fbEventsAccessToken = '';
-$fbEventsLayout = '';
-$fbEventsShowCounter = 'true';
-$fbEventsShowCover = 'true';
-$fbEventsType = 'future';
-$fbEventsYearRange = '1';
-$fbEventsSortation = 'asc';
-$fbEventsStartDate = '';
-$fbEventsEndDate = '';
-
+include ('classes/events.php');
+if (!isset($fbEvents) || (empty($fbEvents)))
+{
+    $fbEvents = new \YAWK\WIDGETS\FACEBOOK\events();
+}
 // $_GET['widgetID'] will be generated in \YAWK\widget\loadWidgets($db, $position)
 if (isset($_GET['widgetID']))
 {
@@ -35,67 +28,152 @@ if (isset($_GET['widgetID']))
             {
                 /* fb app ID */
                 case 'fbEventsAppId';
-                    $fbEventsAppId = $w_value;
+                    $fbEvents->appId = $w_value;
                     break;
 
                 /* fb events page ID  */
                 case 'fbEventsPageId';
-                    $fbEventsPageId = $w_value;
+                    $fbEvents->pageId = $w_value;
                     break;
 
                 /* fb Events Access Token */
                 case 'fbEventsAccessToken';
-                    $fbEventsAccessToken = $w_value;
+                    $fbEvents->accessToken = $w_value;
                     break;
 
                 /* events year range */
                 case 'fbEventsYearRange';
-                    $fbEventsYearRange = $w_value;
+                    $fbEvents->yearRange = $w_value;
                     break;
 
                 /* event type (future, past, all) */
                 case 'fbEventsType';
-                    $fbEventsType = $w_value;
+                    $fbEvents->type = $w_value;
                     break;
 
                 /* show cover if possible */
                 case 'fbEventsShowCover';
-                    $fbEventsShowCover = $w_value;
+                    $fbEvents->showCover = $w_value;
+                    break;
+
+                /* show cover if possible */
+                case 'fbEventsCoverClass';
+                    $fbEvents->coverClass = $w_value;
                     break;
 
                 /* show user counter */
                 case 'fbEventsShowCounter';
-                    $fbEventsShowCounter = $w_value;
+                    $fbEvents->showPeopleCounter = $w_value;
                     break;
 
                 /* events layout (left, top, right, bottom, minimal or table) */
                 case 'fbEventsLayout';
-                    $fbEventsLayout = $w_value;
+                    $fbEvents->layout = $w_value;
                     break;
 
                 /* sortation order (asc or desc) */
                 case 'fbEventsSortation';
-                    $fbEventsSortation = $w_value;
+                    $fbEvents->sortation = $w_value;
                     break;
 
                 /* fixed start date */
                 case 'fbEventsStartDate';
-                    $fbEventsStartDate = $w_value;
+                    $fbEvents->startDate = $w_value;
                     break;
 
                 /* fixed end date */
                 case 'fbEventsEndDate';
-                    $fbEventsEndDate = $w_value;
+                    $fbEvents->endDate = $w_value;
                     break;
 
                 /* heading */
                 case 'fbEventsHeading';
-                    $heading = $w_value;
+                    $fbEvents->heading = $w_value;
                     break;
 
                 /* subtext */
                 case 'fbEventsSubtext';
-                    $subtext = $w_value;
+                    $fbEvents->subtext = $w_value;
+                    break;
+
+                /* font title */
+                case 'fbEventsFontEventName';
+                    $fbEvents->fontEventName = $w_value;
+                    break;
+
+                /* font title css */
+                case 'fbEventsFontEventNameCss';
+                    $fbEvents->fontEventNameCss = $w_value;
+                    break;
+
+                /* font date */
+                case 'fbEventsFontDate';
+                    $fbEvents->fontEventDate = $w_value;
+                    break;
+
+                /* font date css */
+                case 'fbEventsFontDateCss';
+                    $fbEvents->fontEventDateCss = $w_value;
+                    break;
+
+                /* font dateword */
+                case 'fbEventsFontDateword';
+                    $fbEvents->fontEventDateword = $w_value;
+                    break;
+
+                /* font dateword css */
+                case 'fbEventsDatewordCss';
+                    $fbEvents->fontEventDatewordCss = $w_value;
+                    break;
+
+                /* font location */
+                case 'fbEventsFontLocation';
+                    $fbEvents->fontEventLocation = $w_value;
+                    break;
+
+                /* font location css */
+                case 'fbEventsFontLocationCss';
+                    $fbEvents->fontEventLocationCss = $w_value;
+                    break;
+
+                /* font address */
+                case 'fbEventsFontAddress';
+                    $fbEvents->fontEventAddress = $w_value;
+                    break;
+
+                /* font address css */
+                case 'fbEventsFontAddressCss';
+                    $fbEvents->fontEventAddressCss = $w_value;
+                    break;
+
+                /* font desc */
+                case 'fbEventsFontDescription';
+                    $fbEvents->fontEventDescription = $w_value;
+                    break;
+
+                /* font desc css */
+                case 'fbEventsFontDescriptionCss';
+                    $fbEvents->fontEventDescriptionCss = $w_value;
+                    break;
+
+                /* font people */
+                case 'fbEventsFontPeople';
+                    $fbEvents->fontEventPeople = $w_value;
+                    break;
+
+                /* font people css */
+                case 'fbEventsFontPeopleCss';
+                    $fbEvents->fontEventPeopleCss = $w_value;
+                    break;
+
+                /* font canceled */
+                case 'fbEventsFontCanceled';
+                    $fbEvents->fontEventCanceled = $w_value;
+                    break;
+
+                /* font canceled css */
+                case 'fbEventsFontCanceledCss';
+                    $fbEvents->fontEventCanceledCss = $w_value;
                     break;
             }
         } /* END LOAD PROPERTIES */
@@ -103,116 +181,28 @@ if (isset($_GET['widgetID']))
 }
 
 // if a heading is set and not empty
-if (isset($heading) && (!empty($heading)))
+if (isset($fbEvents->heading) && (!empty($fbEvents->heading)))
 {   // add a h1 tag to heading string
-    $heading = "$heading";
+    $fbEvents->heading = "$fbEvents->heading";
 
     // if subtext is set, add <small> subtext to string
-    if (isset($subtext) && (!empty($subtext)))
+    if (isset($fbEvents->subtext) && (!empty($fbEvents->subtext)))
     {   // build a headline with heading and subtext
-        $subtext = "<small>$subtext</small>";
-        $headline = "<h1>$heading&nbsp;"."$subtext</h1>";
+        $fbEvents->subtext = "<small>$fbEvents->subtext</small>";
+        $fbEvents->headline = "<h1>$fbEvents->heading&nbsp;"."$fbEvents->subtext</h1>";
     }
     else
     {   // build just a headline - without subtext
-        $headline = "<h1>$heading</h1>";    // draw just the heading
+        $fbEvents->headline = "<h1>$fbEvents->heading</h1>";    // draw just the heading
     }
 }
 else
 {   // leave empty if it's not set
-    $headline = '';
+    $fbEvents->headline = '';
 }
-if (isset($fbEventsAppId) && (!empty($fbEventsAppId)
-&& (isset($fbEventsAccessToken) && (!empty($fbEventsAccessToken)
-&& (isset($fbEventsPageId) && (!empty($fbEventsPageId)))))))
-{
-    // include facebook SDK JS
-    echo "<script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId      : '".$fbEventsAppId."',
-            xfbml      : true,
-            version    : 'v2.7'
-        });
-        FB.AppEvents.logPageView();
-    };
 
-    (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = \"https://connect.facebook.net/en_US/sdk.js\";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>";
-
-    // WHICH EVENTS TO DISPLAY?
-    // evaluation of event type select field
-    if ($fbEventsType === "all")
-    {
-        // ALL EVENTS (FUTURE + PAST)
-        $since_date = date('Y-01-01', strtotime('-' . $fbEventsYearRange . ' years'));
-        $until_date = date('Y-01-01', strtotime('+' . $fbEventsYearRange . ' years'));
-    }
-    elseif ($fbEventsType == "future")
-    {
-        // UPCOMING EVENTS
-        $since_date = date('Y-m-d');
-        $until_date = date('Y-12-31', strtotime('+' . $fbEventsYearRange . ' years'));
-    }
-    elseif ($fbEventsType === "past")
-    {
-        // PAST EVENTS
-        $since_date = date('Y-01-01', strtotime('-' . $fbEventsYearRange . ' years'));
-        $until_date = date('Y-m-d');
-    }
-    else
-        {   // IF NOT SET - use default:
-            // UPCOMING EVENTS
-            $since_date = date('Y-m-d');
-            $until_date = date('Y-12-31', strtotime('+' . $fbEventsYearRange . ' years'));
-        }
-
-    // IF START + END DATE IS SET
-    if (isset($fbEventsStartDate) && (!empty($fbEventsStartDate))
-    && (isset($fbEventsEndDate) && (!empty($fbEventsEndDate))))
-    {
-        $since_date = date($fbEventsStartDate);
-        $until_date = date($fbEventsEndDate);
-    }
-
-    // unix timestamp years
-    $since_unix_timestamp = strtotime($since_date);
-    $until_unix_timestamp = strtotime($until_date);
-
-    // prepare fields
-    $fields="id,name,description,place,start_time,cover,maybe_count,attending_count,is_canceled";
-
-    // prepare API call
-    $json_link = "https://graph.facebook.com/v2.7/{$fbEventsPageId}/events/attending/?fields={$fields}&access_token={$fbEventsAccessToken}&since={$since_unix_timestamp}&until={$until_unix_timestamp}";
-
-    // get json string
-    $json = file_get_contents($json_link);
-
-    // convert json to object
-    $obj = json_decode($json, true, 512, JSON_BIGINT_AS_STRING);
-
-    // sortation
-    if ($fbEventsSortation === "asc")
-    {   // reverse array data to display upcoming event first
-        $obj['data'] = array_reverse($obj['data']);
-    }
-
-    echo $headline;
-    echo "<pre>";
-    print_r($obj);
-    echo "</pre>";
-
-}
-else
-    {
-        die ("Unable to load Facebook Events. Your app ID, access token or page id is not set correctly!");
-    }
-
+echo $fbEvents->headline;
+$fbEvents->display();
+exit;
 
 ?>
