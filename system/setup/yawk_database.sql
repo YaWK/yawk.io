3cf1aa19dd7b45543b3dcf4ab575615eae92dd57
Daniel Retzl <danielretzl@gmail.com>
2018-07-26 11:20:21 +0200

    issue #182: added field: hashValue to user database

--- system/setup/yawk_database.sql
+++ system/setup/yawk_database.sql
@@ -1796,6 +1796,7 @@ CREATE TABLE `cms_users` (
   `likes` int(11) NOT NULL DEFAULT '0',
   `overrideTemplate` int(1) NOT NULL DEFAULT '1',
   `templateID` int(6) NOT NULL DEFAULT '1'
+  `hashValue` varchar(255) DEFAULT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `cms_users_online` (
