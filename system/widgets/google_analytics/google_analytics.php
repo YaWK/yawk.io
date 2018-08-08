f20cc2174767dd14fa7a6060d2300c3a93e94a4d
Daniel Retzl <danielretzl@gmail.com>
2018-08-09 01:05:31 +0200

    updated google analytics widget code & markup

--- system/widgets/google_analytics/google_analytics.php
+++ system/widgets/google_analytics/google_analytics.php
@@ -1,7 +1,7 @@
 <?php
 // default settings
-$trackingcode = '';
 // $_GET['widgetID'] will be generated in \YAWK\widget\loadWidgets($db, $position)
+$trackingcode = '';
 if (isset($_GET['widgetID']))
 {
 	// widget ID
@@ -9,7 +9,7 @@ if (isset($_GET['widgetID']))
 
 	// get widget settings from db
 	    $res = $db->query("SELECT * FROM {widget_settings}
-	                        WHERE widgetID = '".$wID."'
+	                        WHERE widgetID = '".$widgetID."'
 	                        AND activated = '1'");
    while($row = mysqli_fetch_assoc($res)){
 	      $w_property = $row['property'];
@@ -20,9 +20,13 @@ if (isset($_GET['widgetID']))
 		/* LOAD PROPERTIES */
 		/* TRACKING CODE */
 		if (isset($w_property)){
-			if ($w_property === "trackingcode") {
+			if ($w_property === "gaTrackingcode") {
 				$trackingcode = $w_value;
 			}
+			else
+			    {
+			        $trackingcode = "UA-0000000-00";
+                }
 		}
 	}
 }
@@ -30,15 +34,17 @@ else
 	{
 		$trackingcode = "UA-0000000-00";
 	}
-?>
-<!-- google analytics -->
-<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	ga('create', '<?php echo $trackingcode; ?>', 'auto');
-	ga('require', 'linkid');
-	ga('send', 'pageview');
-</script>
+
+echo"<!-- google analytics -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src=\"https://www.googletagmanager.com/gtag/js?id=$trackingcode\"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '$trackingcode');
+</script>";
+
+?>
