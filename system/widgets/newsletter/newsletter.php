b9c14213d94c36736ad4ef44a2ceab120787b2b4
Daniel Retzl <danielretzl@gmail.com>
2018-08-14 21:37:47 +0200

    issue #195: updated namespace

--- system/widgets/newsletter/newsletter.php
+++ system/widgets/newsletter/newsletter.php
@@ -2,6 +2,6 @@
 // load newsletter class
 require_once ('classes/newsletter.php');
 // create new newsletter widget object
-$newsletter = new \YAWK\WIDGETS\NEWSLETTER\newsletter($db);
+$newsletter = new \YAWK\WIDGETS\NEWSLETTER\SUBSCRIBE\newsletter($db);
 $newsletter->init();
 ?>
