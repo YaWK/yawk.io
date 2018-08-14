9e1fa524aea8dad4ef9708a8a49d785f582ce7a7
Daniel Retzl <danielretzl@gmail.com>
2018-08-14 19:46:54 +0200

    issue #193: added ajax files

---
+++ system/widgets/newsletter/js/nl.js
@@ -0,0 +1,103 @@
+$(document).ready(function(){
+    // RUN AJAX TIMER
+    initTimer();
+    // set a timeout so that ajax calls will be delayed
+    function initTimer(){
+        var myTimer = 0;
+        $("#form").on('keydown', function() {
+            // cancel any previously-set timer
+            if (myTimer) {
+                clearTimeout(myTimer);
+            }
+            // set timeout function
+            myTimer = setTimeout(function() {
+            // call validation settings
+                checkForm();
+            }, 300); //delay time in milliseconds
+            });
+    }
+
+    // check form function
+    function checkForm(){
+        $('#form').validate({ // initialize the plugin
+            // set placement of error messages
+            errorPlacement: function(error, element) {
+            error.insertBefore(element);
+        },
+        rules: {
+        name: {
+            required: true,
+            minlength: 4,
+            maxlength: 48
+            },
+        email: {
+            required: true,
+            email: true,
+            maxlength: 128
+            }
+        }
+        });
+    }
+
+    // on submit
+    $('#submit').click(function(){
+        // get email value from form
+        var	email =	$('#email').val();
+        // get name from form
+        var	name =	$('#name').val();
+        // if no email is set
+        if (!email.trim()) {
+        //    alert('Please insert your email!');
+            // shake the form elements to get users attention
+            $( "#form" ).effect( "shake", {times:2}, 600 );
+            return false;
+        }
+        else
+            {   // run form validator
+                checkForm();
+            }
+
+        // run ajax form
+        $.ajax({
+            // PHP file to process within this request
+            url:'system/widgets/newsletter/js/nl.php',
+            // form method (get or post)
+            type:'post',
+            // allow cross origin requests
+            crossOrigin: true,
+            // data string
+            data:'name='+name+'&email='+email,
+            // check if request was successful
+            success:function(data){
+                // if no data is set
+                if(! data ){
+                    // draw error message and return false
+                    alert('Something went wrong!');
+                    return false;
+                }
+                else    // ajax was successful
+                    {
+                        // hide the complete form
+                        // $("#form").hide();
+                        // $("#formTitle").hide();
+                        $("#formWrapper").hide();
+
+                        // check name to set correct thank you message
+                        if (name)
+                        {   // name is sent, set name as user
+                            user = name;
+                        }
+                        else
+                        {   // name is not sent, set email as user
+                            user = email;
+                        }
+
+                        // append thank you html markup
+                        $("#thankYouMessage").append('<h2>Thank you '+user+' <small>for subscribing to the newsletter!</small></h2>');
+                        // FX: fade in thank you message
+                        $(data).hide().prependTo("#thankYouMessage").fadeIn(820);
+                    }
+            }
+        });
+    });
+});
---
+++ system/widgets/newsletter/js/nl.php
@@ -0,0 +1,68 @@
+<?php
+// start a new session
+session_start();
+// include required classes
+require_once '../../../classes/db.php';
+require_once '../../../classes/alert.php';
+require_once '../../../classes/sys.php';
+
+// check if database is set
+if (!isset($db) || (empty($db)))
+{   // create new db object
+    $db = new \YAWK\db();
+}
+
+// get current datetime
+$now = \YAWK\sys::now();
+
+// check name data
+if (!isset($_POST['name']) || (empty($_POST['name'])))
+{   // name is not set, leave empty
+    $name = "";
+}
+//  name is set
+else
+{   // check if it is a string
+    if (is_string($_POST['name']))
+    {   // email seems to be valid
+        $name = $_POST['name'];
+        // remove html tags
+        $name = strip_tags($name);
+        // quote data
+        $name = $db->quote($name);
+    }
+}
+
+// check email data
+ if (!isset($_POST['email']) || (empty($_POST['email'])))
+{   // email is not set or empty
+    echo "false";
+}
+
+//  email is set
+else
+    {   // validate it
+        if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+        {   // email seems to be valid
+            $email = $_POST['email'];
+            // remove html tags
+            $email = strip_tags($email);
+            // quote data
+            $email = $db->quote($email);
+
+            // insert data into database
+            if ($db->query("INSERT INTO {newsletter} (date_created, name, email) VALUES('".$now."', '".$name."', '".$email."')"))
+            {
+                echo "true";
+            }
+            else
+            {
+                echo "false";
+            }
+        }
+        else
+            {
+                echo "false";
+            }
+    }
+
