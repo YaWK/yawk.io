fdd06a97478dbf69f90e1e08e0db24bf47092397
Daniel Retzl <danielretzl@gmail.com>
2018-08-14 19:49:39 +0200

    issue #195: updated newsletter widget

--- system/widgets/newsletter/newsletter.php
+++ system/widgets/newsletter/newsletter.php
@@ -1,10 +1,7 @@
-<script type="text/javascript" src="system/engines/jquery/jquery.validate.min.js"></script>
-<script type="text/javascript" src="system/engines/jquery/messages_de.min.js"></script>
-<script type="text/javascript" src="system/widgets/newsletter/js/newsletter.js"></script>
-<!-- <span id="newsletterTitle"><i class="fa fa-envelope-o"></i>&nbsp; Newsletter abonieren</span>
-<form class="form-horizontal" id="form" method="post">
-   <input type="text" name="name" id="name" class="form-control" placeholder="Name">
-    <input type="text" name="email" id="email" class="form-control" placeholder="Email">
-    <input type="button" class="btn btn-success" id="submit" name="submit" value="subscribe">
-</form>
--->
+<?php
+// load newsletter class
+require_once ('classes/newsletter.php');
+// create new newsletter widget object
+$newsletter = new \YAWK\WIDGETS\NEWSLETTER\newsletter($db);
+$newsletter->init();
+?>
