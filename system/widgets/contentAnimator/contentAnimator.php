100ab32022fe6394dcb5b0db4685120ecb3e3483
Daniel Retzl <danielretzl@gmail.com>
2018-08-15 04:15:25 +0200

    updated contentAnimator widget

--- system/widgets/contentAnimator/contentAnimator.php
+++ system/widgets/contentAnimator/contentAnimator.php
@@ -1,45 +1,13 @@
 <?php
-// var declaration
-    // how many pixels from object away should the animation start?
-    $contentAnimatorScrollValue = '';
-    // animate.css animation class
-    $contentAnimatorClass = '';
-    // infinite animation loop true|false
-    $contentAnimatorInfinite = '';
+$widget = new \YAWK\widget();
+$data = $widget->getWidgetSettingsArray($db);
+// how many pixels from object away should the animation start?
+$contentAnimatorScrollValue = $data['contentAnimatorScrollValue'];
+// animate.css animation class
+$contentAnimatorClass = $data['contentAnimatorClass'];
+// infinite animation loop true|false
+$contentAnimatorInfinite = $data['contentAnimatorInfinite'];
 
-if (isset($_GET['widgetID']))
-{
-	// widget ID
-	$widgetID = $_GET['widgetID'];
-	/* get widget settings from db */
-	$res = $db->query("SELECT * FROM {widget_settings}
-	                        WHERE widgetID = '".$widgetID."'
-	                        AND activated = '1'");
-	while($row = mysqli_fetch_assoc($res)){
-		$w_property = $row['property'];
-		$w_value = $row['value'];
-		$w_widgetType = $row['widgetType'];
-		$w_activated = $row['activated'];
-		/* end of get widget settings */
-
-		/* LOAD PROPERTIES */
-		if (isset($w_property)){
-			switch($w_property)
-			{
-				case 'contentAnimatorScrollValue';
-					$contentAnimatorScrollValue = $w_value;
-					break;
-				case 'contentAnimatorClass';
-					$contentAnimatorClass = $w_value;
-					break;
-                case 'contentAnimatorInfinite';
-                    $contentAnimatorInfinite = $w_value;
-                    break;
-			}
-		} /* END LOAD PROPERTIES */
-
-	} // end while fetch row (fetch widget settings)
-} // if no widget ID is given or settings could not be retrieved, use this as defaults:
 
 // check if class is empty
 if (!isset($contentAnimatorClass) || (empty($contentAnimatorClass)))
@@ -72,7 +40,7 @@ else
 // echo "<script src=\"system/engines/velocityJS/velocity-ui.min.js\"></script>";
 
 // load css with animation definitions
-echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"system/widgets/contentAnimator/animate.min.css\">";
+echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"system/widgets/contentAnimator/animate.css\">";
 // echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"system/engines/animateCSS/animate.min.css\">";
 // echo "<style type=\"text/css\">.animate {visibility:hidden;}</style>";
 // output javsscript
