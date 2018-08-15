8dbf9989c9d148fde10541b9ae168c07d25cf292
Daniel Retzl <danielretzl@gmail.com>
2018-08-15 15:05:59 +0200

    issue #198: refactored clock widget: added class, language tags and oop code

---
+++ system/widgets/clock/classes/clock.php
@@ -0,0 +1,119 @@
+<?php
+namespace YAWK\WIDGETS\CLOCK\CURRENT
+{
+    /**
+     * <b>Embed a digital clock on your website.</b>
+     *
+     * <p>The clock shows the current time. (wonder, oh wonder...) You can set the clock's font color,
+     * alignment (left, center or right) as well as a custom css class to design it and put some FX on
+     * if you need to. If you need an analog clock anywhere on your website, this widget does the job.
+     *
+     * @package    YAWK
+     * @author     Daniel Retzl <danielretzl@gmail.com>
+     * @copyright  2018 Daniel Retzl
+     * @license    https://opensource.org/licenses/MIT
+     * @version    1.0.0
+     * @link       http://yawk.io
+     * @annotation Embed a digital real-time clock on your website.
+     */
+    class clock
+    {
+        /** @var object global widget object data */
+        public $widget = '';
+        /** @var string Clock color */
+        public $clockColor = '#f5f5f5';
+        /** @var string Clock alignment */
+        public $clockAlign = 'text-center';
+        /** @var string Clock class */
+        public $clockClass = 'h1';
+
+        /**
+         * Load all widget settings from database and fill object
+         * @author Daniel Retzl <danielretzl@gmail.com>
+         * @version 1.0.0
+         * @link http://yawk.io
+         * @param object $db Database Object
+         * @annotation Load all widget settings on object init.
+         */
+        public function __construct($db)
+        {
+            // load this widget settings from db
+            $this->widget = new \YAWK\widget();
+            $settings = $this->widget->getWidgetSettingsArray($db);
+            foreach ($settings as $property => $value) {
+                $this->$property = $value;
+            }
+        }
+
+        /**
+         * Print all object data
+         * @author Daniel Retzl <danielretzl@gmail.com>
+         * @version 1.0.0
+         * @link http://yawk.io
+         * @annotation (for development and testing purpose)
+         */
+        public function printObject()
+        {
+            echo "<pre>";
+            print_r($this);
+            echo "</pre>";
+        }
+
+        /**
+         * Initialize: prepare JS and start the clock
+         * @author Daniel Retzl <danielretzl@gmail.com>
+         * @version 1.0.0
+         * @link http://yawk.io
+         * @annotation use this method to run the clock
+         */
+        public function init()
+        {
+            $this->loadJavascript();
+            $this->drawClock();
+        }
+
+        /**
+         * Load required javascript
+         * @author Daniel Retzl <danielretzl@gmail.com>
+         * @version 1.0.0
+         * @link http://yawk.io
+         * @annotation The engine behind this clock
+         */
+        public function loadJavascript()
+        {
+            echo"<body onload=\"clock();\">
+            <script type=\"text/javascript\">
+            function clock() {
+                var now = new Date();
+                var minute = now.getMinutes();
+                var second= now.getSeconds();
+                var hour = now.getHours();
+                if(minute<10) minute = \"0\" + minute;
+                if(second<10) second= \"0\" + second;
+                if(hour<10) hour=\"0\" + hour;
+                var outStr = hour+':'+minute+':'+second;
+                /*   output string without leading zero int
+                 *   var outStr = now.getHours()+':'+now.getMinutes()+':'+now.getSeconds(); */
+                document.getElementById('clockDiv').innerHTML=outStr;
+                setTimeout('clock()',1000);
+            }
+            clock();
+            </script>";
+        }
+
+        /**
+         * Draw the clock on screen
+         * @author Daniel Retzl <danielretzl@gmail.com>
+         * @version 1.0.0
+         * @link http://yawk.io
+         * @annotation This method draws the clock on screen
+         */
+        public function drawClock()
+        {
+            echo"
+            <div class=\"$this->clockAlign\">
+                <div id=\"clockDiv\" class=\"$this->clockClass\" style=\"color:#$this->clockColor\"></div>
+            </div>";
+        }
+    }
+}
--- system/widgets/clock/clock.php
+++ system/widgets/clock/clock.php
@@ -1,30 +1,16 @@
 <?php
-$widget = new \YAWK\widget();
-$data = $widget->getWidgetSettingsArray($db);
-$color = $data['clockColor'];
-$align = $data['clockAlign'];
-$class = $data['clockClass'];
-?>
-
-<body onload="clock();">
-<script type="text/javascript">
-function clock() {
-   var now = new Date();
-   var minute = now.getMinutes();
-   var second= now.getSeconds();
-   var hour = now.getHours();
-    if(minute<10) minute = "0" + minute;
-    if(second<10) second= "0" + second;
-    if(hour<10) hour="0" + hour;
-   var outStr = hour+':'+minute+':'+second;
-/*   output string without leading zero int
- *   var outStr = now.getHours()+':'+now.getMinutes()+':'+now.getSeconds(); */
-   document.getElementById('clockDiv').innerHTML=outStr;
-   setTimeout('clock()',1000);
+// check if db obj exits
+if (!isset($db) || (empty($db)))
+{   // if not, create new db obj
+    $db = new \YAWK\db();
 }
-clock();
-</script>  
-
-<div class="<?php echo $align; ?>">
-	<div id="clockDiv" class="<?php echo $class; ?>" style="color:<?php echo "#"."$color;"; ?>"></div>
-</div>
+// check if clock obj is loaded
+if(!isset($clock) || (empty($clock)))
+{
+    // if not, include clock widget class
+    require_once ('classes/clock.php');
+    // create clock widget object
+    $clock = new \YAWK\WIDGETS\CLOCK\CURRENT\clock($db);
+}
+// init current clock
+$clock->init();
