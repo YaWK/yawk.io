9e2c83f0d564722530c60a526427251ebf83c7f4
Daniel Retzl <danielretzl@gmail.com>
2018-08-15 03:50:21 +0200

    updated bubblus widget

--- system/widgets/bubblus/bubblus.php
+++ system/widgets/bubblus/bubblus.php
@@ -1,61 +1,12 @@
 <?php
-// set default values
-$bubblusUrl="https://bubbl.us/NDAxNjg1Mi83OTM0MjA4LzJhMTViYjE0MDhmN2ZjNjgxZTA3Mjc3YjdjYWY4MDM2-X?s=7934208";
-$bubblusWidth = "100%";
-$bubblusHeight = "600";
-$heading = '';
-$subtext = '';
+$widget = new \YAWK\widget();
+$data = $widget->getWidgetSettingsArray($db);
+$bubblusUrl = $data['bubblusUrl'];
+$bubblusWidth = $data['bubblusWidth'];
+$bubblusHeight = $data['bubblusHeight'];
+$heading = $data['bubblusHeading'];
+$subtext = $data['bubblusSubtext'];
 
-// $_GET['widgetID'] will be generated in \YAWK\widget\loadWidgets($db, $position)
-if (isset($_GET['widgetID']))
-{
-    // widget ID
-    $widgetID = $_GET['widgetID'];
-
-    // get widget settings from db
-    $res = $db->query("SELECT * FROM {widget_settings}
-	                        WHERE widgetID = '".$widgetID."'
-	                        AND activated = '1'");
-    while($row = mysqli_fetch_assoc($res))
-    {   // set widget properties and values into vars
-        $w_property = $row['property'];
-        $w_value = $row['value'];
-        $w_widgetType = $row['widgetType'];
-        $w_activated = $row['activated'];
-        /* end of get widget properties */
-
-        /* filter and load those widget properties */
-        if (isset($w_property)){
-            switch($w_property)
-            {
-                /* url of the video to stream */
-                case 'bubblusUrl';
-                    $bubbleusUrl = $w_value;
-                    break;
-
-                /* width */
-                case 'bubblusWidth';
-                    $bubblusWidth = $w_value;
-                    break;
-
-                /* height */
-                case 'bubblusHeight';
-                    $bubblusHeight = $w_value;
-                    break;
-
-                /* heading */
-                case 'bubblusHeading';
-                    $heading = $w_value;
-                    break;
-
-                /* subtext */
-                case 'bubblusSubtext';
-                    $subtext = $w_value;
-                    break;
-            }
-        } /* END LOAD PROPERTIES */
-    } // end while fetch row (fetch widget settings)
-}
 
 // if a heading is set and not empty
 if (isset($heading) && (!empty($heading)))
