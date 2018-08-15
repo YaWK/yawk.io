8617b573a1faab1e5243ce819dc001022c1d1421
Daniel Retzl <danielretzl@gmail.com>
2018-08-15 03:54:30 +0200

    updated chaturbate widget

--- system/widgets/chaturbate/chaturbate.php
+++ system/widgets/chaturbate/chaturbate.php
@@ -1,73 +1,14 @@
 <?php
-// set default values
-$chaturbateRoom = "yourRoom";
-$disableSound = "1";
-$embedVideoOnly = "0";
-$width = "523";
-$height = "850";
-$heading='';
-$subtext='';
-
-// $_GET['widgetID'] will be generated in \YAWK\widget\loadWidgets($db, $position)
-if (isset($_GET['widgetID']))
-{
-    // widget ID
-    $widgetID = $_GET['widgetID'];
-
-    // get widget settings from db
-    $res = $db->query("SELECT * FROM {widget_settings}
-	                        WHERE widgetID = '".$widgetID."'
-	                        AND activated = '1'");
-    while($row = mysqli_fetch_assoc($res))
-    {   // set widget properties and values into vars
-        $w_property = $row['property'];
-        $w_value = $row['value'];
-        $w_widgetType = $row['widgetType'];
-        $w_activated = $row['activated'];
-        /* end of get widget properties */
-
-        /* filter and load those widget properties */
-        if (isset($w_property)){
-            switch($w_property)
-            {
-                /* url of the video to stream */
-                case 'chaturbateRoom';
-                    $chaturbateRoom = $w_value;
-                    break;
-
-                /* disable sound 0|1 default: 1 */
-                case 'chaturbateDisableSound';
-                    $disableSound = $w_value;
-                    break;
-
-                /* video only? 0|1 default: 0 */
-                case 'chaturbateVideoOnly';
-                    $embedVideoOnly = $w_value;
-                    break;
-
-                /* iframe height in px */
-                case 'chaturbateHeight';
-                    $height = $w_value;
-                    break;
-
-                /* iframe width in px */
-                case 'chaturbateWidth';
-                    $width = $w_value;
-                    break;
-
-                /* heading */
-                case 'chaturbateHeading';
-                    $heading = $w_value;
-                    break;
-
-                /* subtext */
-                case 'chaturbateSubtext';
-                    $subtext = $w_value;
-                    break;
-            }
-        } /* END LOAD PROPERTIES */
-    } // end while fetch row (fetch widget settings)
-}
+$widget = new \YAWK\widget();
+$data = $widget->getWidgetSettingsArray($db);
+$chaturbateRoom = $data['chaturbateRoom'];
+$disableSound = $data['chaturbateDisableSound'];
+$embedVideoOnly = 0;
+$width = $data['chaturbateWidth'];
+$height = $data['chaturbateHeight'];
+$width = $data['chaturbateVideoOnly'];
+$heading = $data['bubblusHeading'];
+$subtext = $data['bubblusSubtext'];
 
 // if a heading is set and not empty
 if (isset($heading) && (!empty($heading)))
