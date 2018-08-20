258126c84c420f1d3e30a1d36017f11e66b4ac58
Daniel Retzl <danielretzl@gmail.com>
2018-08-21 00:29:11 +0200

    issue #213: refactored / improved markup

--- system/widgets/gallery/gallery.php
+++ system/widgets/gallery/gallery.php
@@ -1,16 +1,11 @@
 <?php
-if (!isset($db))
-{   // get db object
-    require_once 'system/classes/db.php';
-    $db = new \YAWK\db();
-}
+/** @var $db \YAWK\db */
 /** GALLERY PLUGIN  */
 if (!isset($gallery))
-{   // load plugin gallery class
+{   // load gallery widget class
     require_once 'classes/gallery.php';
-    // create new gallery obj
+    // create new gallery widget object
     $gallery = new \YAWK\WIDGETS\GALLERY\IMAGES\gallery($db);
 }
 // embed image gallery
 $gallery->init($db);
-?>
