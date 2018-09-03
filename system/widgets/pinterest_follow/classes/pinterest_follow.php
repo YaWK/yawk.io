3238213c573eef2ab2dc19de619da6c69f24bf8f
dretzl <danielretzl@gmail.com>
2018-09-03 23:23:52 +0200

    issue #226: refactored pinterest follow widget

---
+++ system/widgets/pinterest_follow/classes/pinterest_follow.php
@@ -0,0 +1,70 @@
+<?php
+namespace YAWK\WIDGETS\PINTEREST\FOLLOW
+{
+    /**
+     * <b>Pinterest Follow Widget - add a follow me on pinterest button to your page.</b>
+     *
+     * <p>If you want to pay attention to your pinterest channel, embed this widget.
+     * All you need to enter is any pinterest url to follow. Like any other widget,
+     * you can add a heading and a subtext that will be displayed above this widget.</p>
+     *
+     * @package    YAWK
+     * @author     Daniel Retzl <danielretzl@gmail.com>
+     * @copyright  2018 Daniel Retzl
+     * @license    https://opensource.org/licenses/MIT
+     * @version    1.0.0
+     * @link       http://yawk.io
+     * @annotation Pinterest Follow Widget
+     */
+    class pinterestFollow extends \YAWK\widget
+    {
+        /** @var object global widget object data */
+        public $widget = '';
+        /** @var string Title that will be shown above widget */
+        public $exampleHeading = '';
+        /** @var string Subtext will be displayed beside title */
+        public $exampleSubtext = '';
+        /** @var string The URL of your pinterest channel */
+        public $pinterestFollowUrl = '';
+
+        /**
+         * Load all widget settings from database and fill object
+         * @author Daniel Retzl <danielretzl@gmail.com>
+         * @version 1.0.0
+         * @link http://yawk.io
+         * @param object $db Database Object
+         * @annotation Load all widget settings on object init.
+         */
+        public function __construct($db)
+        {
+            // load this widget settings from db
+            $this->widget = new \YAWK\widget();
+            $settings = $this->widget->getWidgetSettingsArray($db);
+            foreach ($settings as $property => $value) {
+                $this->$property = $value;
+            }
+        }
+
+        /**
+         * Init example widget and call a function for demo purpose
+         * @author Daniel Retzl <danielretzl@gmail.com>
+         * @version 1.0.0
+         * @link http://yawk.io
+         * @annotation Example Widget Init
+         */
+        public function init()
+        {   // draw headline on screen
+            $this->getHeading($this->exampleHeading, $this->exampleSubtext);
+            $this->embedPinterestFollow();
+        }
+
+        public function embedPinterestFollow()
+        {
+            echo '
+            <a data-pin-do="buttonFollow" href="'.$this->pinterestFollowUrl.'">Pinterest</a>
+            <script async defer src="//assets.pinterest.com/js/pinit.js"></script>';
+        }
+
+    }
+}
+?>
