45da8ba73fb47c0118353b30f4fc0eb3c8bf94ed
dretzl <danielretzl@gmail.com>
2018-09-14 00:23:12 +0200

    issue #48: added twitter timeline grid widget class

---
+++ system/widgets/twitter_grid/classes/twitterGrid.php
@@ -0,0 +1,73 @@
+<?php
+namespace YAWK\WIDGETS\TWITTER\GRID
+{
+    /**
+     * <b>Twitter Widget - embed Twitter Timeline as grid</b>
+     *
+     * <p>Twitter Timelines can be embeded as grid view. All you need is
+     * the URL of your twitter timeline and the amount of items you wish
+     * to embed. You will get the latest tweets.</p>
+     *
+     * @package    YAWK
+     * @author     Daniel Retzl <danielretzl@gmail.com>
+     * @copyright  2018 Daniel Retzl
+     * @license    https://opensource.org/licenses/MIT
+     * @version    1.0.0
+     * @link       http://yawk.io
+     * @annotation Embed Twitter Timeline as grid
+     */
+    class twitterGrid extends \YAWK\widget
+    {
+        /** @var object global widget object data */
+        public $widget = '';
+        /** @var string Title that will be shown above widget */
+        public $twitterGridHeading = '';
+        /** @var string Subtext will be displayed beside title */
+        public $twitterGridSubtext = '';
+        /** @var string URL of your twitter timeline */
+        public $twitterGridUrl = "https://twitter.com/TwitterDev/timelines/539487832448843776";
+        /** @var string How many tweets (latest n) */
+        public $twitterGridTweetLimit = "5";
+
+        /**
+         * Load all widget settings from database and fill object
+         * @author Daniel Retzl <danielretzl@gmail.com>
+         * @version 1.0.0
+         * @link http://yawk.io
+         * @param object $db Database Object
+         * @annotation Load all widget settings on object init.
+         */
+        public function __construct($db)
+        {
+            // load this widget settings from db
+            $this->widget = new \YAWK\widget();
+            $settings = $this->widget->getWidgetSettingsArray($db);
+            foreach ($settings as $property => $value) {
+                $this->$property = $value;
+            }
+        }
+
+        /**
+         * Init Twitter widget
+         * @author Daniel Retzl <danielretzl@gmail.com>
+         * @version 1.0.0
+         * @link http://yawk.io
+         * @annotation Twitter Widget Init
+         */
+        public function init()
+        {   // display heading
+            echo $this->getHeading($this->twitterGridHeading, $this->twitterGridSubtext);
+
+            // remove trailing slash from url
+            $this->twitterGridUrl = rtrim($this->twitterGridUrl,"/");
+            // explode user from URL
+            $twitterUserArray = explode("/", $this->twitterGridUrl);
+            $twitterUser = $twitterUserArray['3'];
+
+echo "<a class=\"twitter-grid\" data-limit=\"$this->twitterGridTweetLimit\" href=\"$this->twitterGridUrl\">$twitterUser Tweets</a>
+<script async src=\"//platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>";
+
+        }
+    }
+}
+?>
