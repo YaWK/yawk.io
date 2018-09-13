a62faeff262ed7f9421d12fc2f848af840ecc4f5
dretzl <danielretzl@gmail.com>
2018-09-13 23:04:57 +0200

    issue #4: updated YouTube widget index page

--- system/widgets/youtube/youtube.php
+++ system/widgets/youtube/youtube.php
@@ -1,16 +1,11 @@
 <?php
-// check if db obj exits
-if (!isset($db) || (empty($db)))
-{   // if not, create new db obj
-    $db = new \YAWK\db();
-}
+/** @var $db \YAWK\db */
 // check if youtube obj is loaded
 if(!isset($youtube) || (empty($youtube)))
-{
-    // if not, include yt widget class
+{   // not set, include yt widget class
     require_once ('classes/youtube.php');
     // create yt widget object
     $youtube = new \YAWK\WIDGETS\YOUTUBE\VIDEO\youtube($db);
 }
-// embed yt Video
+// embed yt video
 $youtube->embedVideo();
