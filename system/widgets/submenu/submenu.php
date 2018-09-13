e82d0f90aa9384ae30edfbbe3abb582e618bb79b
dretzl <danielretzl@gmail.com>
2018-09-13 20:37:48 +0200

    issue #246: updated submenu widget index page

--- system/widgets/submenu/submenu.php
+++ system/widgets/submenu/submenu.php
@@ -1,44 +1,10 @@
 <?php
-// set default values
-$menuID = 0;
-
-// $_GET['widgetID'] will be generated in \YAWK\widget\loadWidgets($db, $position)
-if (isset($_GET['widgetID']))
-{
-    // widget ID
-    $widgetID = $_GET['widgetID'];
-
-    // get widget settings from db
-    $res = $db->query("SELECT * FROM {widget_settings}
-	                        WHERE widgetID = '".$widgetID."'
-	                        AND activated = '1'");
-    while($row = mysqli_fetch_assoc($res))
-    {   // set widget properties and values into vars
-        $w_property = $row['property'];
-        $w_value = $row['value'];
-        $w_widgetType = $row['widgetType'];
-        $w_activated = $row['activated'];
-        /* end of get widget properties */
-
-        /* filter and load those widget properties */
-        if (isset($w_property)){
-            switch($w_property)
-            {
-                /* name of the menu to show */
-                case 'menuID';
-                    $menuID = $w_value;
-                    break;
-            }
-        } /* END LOAD PROPERTIES */
-    } // end while fetch row (fetch widget settings)
+/** @var $db \YAWK\db */
+if (!isset($submenu))
+{   // load submenu widget class
+    require_once 'classes/submenu.php';
+    // create new submenu widget object
+    $submenu = new \YAWK\WIDGETS\SUBMENU\EMBED\submenu($db);
 }
-
-// HTML output
-if (!isset($menu))
-{
-    @require_once 'system/classes/menu.php';
-    $menu = new \YAWK\menu();
-}
-// echo "<div class=\"row\">";
-$menu->displaySubMenu($db, $menuID);
-// echo "</div>";
+// init submenu widget
+$submenu->init($db);
