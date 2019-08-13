9decc0483374bd74b0fe099aec686938acebb17c
dretzl <danielretzl@gmail.com>
2019-08-13 20:41:58 +0200

    updated index page (wip)

--- content/pages/index.php
+++ content/pages/index.php
@@ -1,6 +1,8 @@
     
-<div class="col-md-8">
+    
+<div class="col-sm-12">
         <h1>YAWK! :: Yet another WebKit</h1><br>  
+        <img src="media/images/deviceTypes.jpg" id="globe" class="img-fluid rounded float-right">
     	<a href="http://yawk.io" target="_blank">YaWK</a> is a bundle of the most successful opensource web solutions. Weld together to build, design and operate any
         modern website. The Backend got a bootstrap layout builder that allows you to design a versatile bootrap grid-based
         layout with a just few clicks. Add and organize Pages, Articles, Menu Entries. Extend your page with big Plugins or 
@@ -10,30 +12,7 @@
         Put together, what belongs together. YaWK bundles Bootstrap, jQuery, AdminLTE, FontAwesome and much more of your favourite 
         "daily use" web(open)sourceware into a new atop overall CMS.
     <small>small text</small> 
-	<br><br>
 </div>
 
-    <div class="col-md-4">
-        
-        <img src="media/images/deviceTypes.jpg" id="globe" class="img-rounded">
-    </div>
-
-
-<p class="font-weight-bold">Bold text.</p>
-<p class="font-weight-normal">Normal weight text.</p>
-<p class="font-weight-light">Light weight text.</p>
-<p class="text-monospace">Monospace text.</p>    
-     
-<div class="container">
-  <div class="row">
-    <div class="col text-center">
-      One of three columns
-    </div>
-    <div class="col text-center">
-      One of three columns
-    </div>
-    <div class="col text-center">
-      One of three columns
-    </div>
-  </div>
-</div>         
+ 
+                  
