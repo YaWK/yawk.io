8cf657950c038edd216ef1b87897a5dafa2a752d
dretzl <danielretzl@gmail.com>
2019-08-25 03:43:29 +0200

    issue #319: added blog widget class

---
+++ system/widgets/news/classes/blogWidget.php
@@ -0,0 +1,158 @@
+<?php
+namespace YAWK\WIDGETS\BLOG\WIDGET {
+    /**
+     * <b>Embed plugin: blog</b>
+     *
+     * <p>This is the widget to the blog plugin. With this widget you can embed any blog that you
+     * you've created before using the blog plugin. Simply set the ID of the blog you would like
+     * to embed, setup your settings, choose the position and page where you like to display your
+     * blog and you're good to go. </p>
+     *
+     * @package    YAWK
+     * @author     Daniel Retzl <danielretzl@gmail.com>
+     * @copyright  2019 Daniel Retzl
+     * @license    https://opensource.org/licenses/MIT
+     * @version    1.0.0
+     * @link       http://yawk.io
+     * @annotation Embed a blog on your website.
+     */
+    class blogWidget extends \YAWK\widget
+    {
+        /** @var object global widget object data */
+        public $widget = '';
+        /** @var int Gallery ID */
+        public $blogID = '';
+        /** @var string Title that will be shown above widget */
+        public $blogHeading = '';
+        /** @var string Subtext will be displayed beside title */
+        public $blogSubtext = '';
+        /** @var int Limit Entries (if 0 all items will be displayed)
+         * This can overrule the global blog setting and has only impact
+         * to the specific blog widget */
+        public $blogLimitEntries = '';
+
+        // for more specific settings take a look at the blog plugin
+
+        /**
+         * Load all widget settings from database and fill object
+         * @author Daniel Retzl <danielretzl@gmail.com>
+         * @version 1.0.0
+         * @link http://yawk.io
+         * @param object $db Database Object
+         * @annotation Load all widget settings on object init.
+         */
+        public function __construct($db)
+        {
+            // load this widget settings from db
+            $this->widget = new \YAWK\widget();
+            $settings = $this->widget->getWidgetSettingsArray($db);
+            foreach ($settings as $property => $value) {
+                $this->$property = $value;
+            }
+        }
+
+        /**
+         * Print all object data
+         * @author Daniel Retzl <danielretzl@gmail.com>
+         * @version 1.0.0
+         * @link http://yawk.io
+         * @annotation (for development and testing purpose)
+         */
+        public function printObject()
+        {
+            echo "<pre>";
+            print_r($this);
+            echo "</pre>";
+        }
+
+        /**
+         * Init Gallery and load methods
+         * @author Daniel Retzl <danielretzl@gmail.com>
+         * @version 1.0.0
+         * @link http://yawk.io
+         * @annotation load Javascript and draw image gallery
+         */
+        public function init($db)
+        {
+            // $this->printObject();
+            $this->drawBlogWidget($db);
+        }
+
+
+        /**
+         * Draw the blog
+         * @author Daniel Retzl <danielretzl@gmail.com>
+         * @version 1.0.0
+         * @link http://yawk.io
+         * @param db object Database object
+         * @annotation (for development and testing purpose)
+         */
+        public function drawBlogWidget($db)
+        {
+            /** @var $db \YAWK\db * */
+            error_reporting(1);
+            if (!isset($this->blogID) || (empty($this->blogID))) {
+                echo "Error: unable to load blog because there was no blog ID given.";
+            } else {
+                require_once 'system/plugins/blog/classes/blog.php';
+
+
+            echo "<script type=\"text/javascript\" src=\"../../../plugins/blog/js/comments.js\"></script>
+                  <script type=\"text/javascript\" src=\"../../../plugins/blog/js/voting.js\"></script>";
+
+
+                /*
+                 * FRONTEND PAGE
+                 */
+                $blog = new \YAWK\PLUGINS\BLOG\blog();
+                // print_r($blog);
+                /* get templateID */
+                $templateID = \YAWK\template::getCurrentTemplateId($db);
+
+                /* a blog can be called by GET variable or via page include */
+                $blog->blogid = $this->blogID;
+
+                // get published status (is it online or offline?)
+                $published = $blog->getBlogProperty($db, $blog->blogid, "published");
+                // get group id of this blog
+                $gid = $blog->getBlogProperty($db, $blog->blogid, "gid");
+                // get limit entries (number of items to display)
+                $blog->limitEntries = $blog->getBlogProperty($db, $blog->blogid, "limitEntries");
+
+                // if blog is not offline, get entries from db + draw it on screen.
+                if ($published != 0) {
+                    /** @var $widget \YAWK\widget */
+                    // get headline
+                    $this->headline = $this->getHeading($this->blogHeading, $this->blogSubtext);
+
+                    if (!isset($item_id)) {
+                        $item_id = 0;
+                    }
+
+                    // load blog title
+                    $blog->getTitle($db, $blog->blogid);
+
+                    // check if full view is set
+                    if (!isset($full_view)) {
+                        $full_view = 0;
+                    }
+
+                    // load the blog entries into blog object
+                    $blog->getFrontendEntries($db, $blog->blogid, $item_id, $full_view, $this->blogLimitEntries);
+                    // check footer setting and load it on demand
+                    if ($blog->getBlogProperty($db, $blog->blogid, "footer")) {
+                        $blog->getFooter($db);
+                    }
+                    // finally: draw the blog
+                    print $blog->draw();
+                    print "</div>";
+                }
+                else
+                    {   // blog is not published, draw message
+                        echo \YAWK\alert::draw("warning", "Entschuldigung!", "Dieser Bereich ist im Moment offline, da gerade daran gearbeitet wird. Bitte komm sp&auml;ter wieder.", "", "4800");
+                    }
+            }
+        }
+    }
+}
+?>
--- system/widgets/news/news.php
+++ system/widgets/news/news.php
@@ -1,13 +1,11 @@
-<div class="row">
-    <div class="col-md-4 text-center">
-        <h1><i class="fa fa-newspaper-o"></i> News <small>...aus meiner Welt</small></h1>
-        <img src="media/images/360/121.jpg" class="img-thumbnail protected"><br><br><br>
-        <img src="media/images/string-butt.jpg" class="img-thumbnail protected">
-    </div>
-    <div class="col-md-8"><br>
-        <?php
-        // load news blog
-        $blog_id = 4; $limit = 2; include 'system/plugins/blog/blog.php';
-        ?>
-    </div>
-</div>
+<?php
+/** @var $db \YAWK\db */
+/** BLOG PLUGIN  */
+if (!isset($blogWidget))
+{   // load blog widget class
+    require_once 'classes/blogWidget.php';
+    // create new blog widget object
+    $blogWidget = new \YAWK\WIDGETS\BLOG\WIDGET\blogWidget($db);
+}
+// embed blog
+$blogWidget->init($db);
