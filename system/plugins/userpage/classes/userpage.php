53bd2c0330b7006efce617fcdc61fee713978801
dretzl <danielretzl@gmail.com>
2019-10-11 16:58:27 +0200

    issue #343: updated tabs to bootstrap 4

--- system/plugins/userpage/classes/userpage.php
+++ system/plugins/userpage/classes/userpage.php
@@ -201,24 +201,6 @@ namespace YAWK\PLUGINS\USERPAGE {
                       col 2
                       </div>
                     </div>";
-            // }
-
-            // ADDITIONAL CODE ------------------
-            // IF ADMINs SHOULD GET THEIR OWN WELCOME-USERPAGE
-            // ELSE CODE FOR DEFAULT USERS....
-            /*
-            else  {
-                $i = 42;
-                $t = 89;
-                $y = 136;
-                $dashboard = "
-                    <div class=\"row\">
-                      <div class=\"col-md-4\"><h4><i class=\"fa fa-envelope-o fa-2x animated infinite pulse text-info\"></i> &nbsp;".$i." neue Nachrichten</h4></div>
-                      <div class=\"col-md-4\"><h4><i class=\"fa fa-heart fa-2x animated infinite pulse text-danger\"></i> &nbsp;Du hast ".$t." Likes</h4></div>
-                      <div class=\"col-md-4\"><h4><i class=\"fa fa-user fa-2x text-success\"></i> &nbsp;".$y." wurden heute auf dich aufmerksam</h4></div>
-                    </div>";
-            }
-            */
 
             $html = "";
             $signup_hellotextsub = \YAWK\settings::getSetting($db, "userpage_hellotextsub");
@@ -240,33 +222,33 @@ namespace YAWK\PLUGINS\USERPAGE {
           <ul class=\"nav nav-tabs\" role=\"tablist\">";
             // dashboard TAB
             if (\YAWK\settings::getSetting($db, "userpage_dashboard") === '1'){
-               $html .= "<li role=\"presentation\" $activeDashboardTab><a href=\"#home\" aria-controls=\"home\" role=\"tab\" data-toggle=\"tab\">
+               $html .= "<li class=\"nav-item\" role=\"presentation\" $activeDashboardTab><a href=\"#home\" class=\"nav-link active\" aria-controls=\"home\" role=\"tab\" data-toggle=\"tab\">
                         <i class=\"fa fa-home\"></i>&nbsp; Userpage</a></li>";
             }
             // profile TAB
             if (\YAWK\settings::getSetting($db, "userpage_profile") === '1') {
-                $html .= "<li role=\"presentation\" $activeProfileTab><a href=\"#profile\" aria-controls=\"profile\" role=\"tab\" data-toggle=\"tab\">
+                $html .= "<li class=\"nav-item\" role=\"presentation\" $activeProfileTab><a href=\"#profile\" class=\"nav-link\" aria-controls=\"profile\" role=\"tab\" data-toggle=\"tab\">
                          <i class=\"fa fa-user\"></i>&nbsp; Edit Profile</a></li>";
             }
             // messages TAB
             if (\YAWK\settings::getSetting($db, "userpage_msgplugin") === '1') {
-                $html .="<li role = \"presentation\" $activeMessagesTab><a href=\"#messages\" aria-controls=\"messages\" role=\"tab\" data-toggle=\"tab\">
+                $html .="<li class=\"nav-item\" role = \"presentation\" $activeMessagesTab><a href=\"#messages\" class=\"nav-link\"  aria-controls=\"messages\" role=\"tab\" data-toggle=\"tab\">
                         <i class=\"fa fa-envelope\"></i>&nbsp; Messages</a></li>";
             }
             // settings TAB
             if (\YAWK\settings::getSetting($db, "userpage_settings") === '1') {
-                $html .= "<li role=\"presentation\" $activeSettingsTab><a href=\"#settings\" aria-controls=\"settings\" role=\"tab\" data-toggle=\"tab\">
+                $html .= "<li class=\"nav-item\" role=\"presentation\" $activeSettingsTab><a href=\"#settings\" class=\"nav-link\" aria-controls=\"settings\" role=\"tab\" data-toggle=\"tab\">
                 <i class=\"fa fa-cog\"></i>&nbsp; Settings</a></li>";
             }
             // stats TAB
             if (\YAWK\settings::getSetting($db, "userpage_stats") === '1') {
-                $html .= "<li role=\"presentation\" $activeStatsTab><a href=\"#stats\" aria-controls=\"stats\" role=\"tab\" data-toggle=\"tab\">
+                $html .= "<li class=\"nav-item\" role=\"presentation\" $activeStatsTab><a href=\"#stats\" class=\"nav-link\" aria-controls=\"stats\" role=\"tab\" data-toggle=\"tab\">
                 <i class=\"fa fa-line-chart\"></i>&nbsp; Stats</a></li>";
             }
             // help TAB
             if (\YAWK\settings::getSetting($db, "userpage_help") === '1') {
                 $signup_help = 1;
-                $html .= "<li role=\"presentation\" $activeHelpTab><a href=\"#help\" aria-controls=\"help\" role=\"tab\" data-toggle=\"tab\">
+                $html .= "<li class=\"nav-item\" role=\"presentation\" $activeHelpTab><a href=\"#help\" class=\"nav-link\" aria-controls=\"help\" role=\"tab\" data-toggle=\"tab\">
                 <i class=\"fa fa-question-circle\"></i>&nbsp; Help</a></li>";
             }
             // append TAB
@@ -298,7 +280,7 @@ namespace YAWK\PLUGINS\USERPAGE {
                 // message plugin include
                 include 'system/plugins/messages/classes/messages.php';
                 $messages = new \YAWK\PLUGINS\MESSAGES\messages($db, "frontend");
-                $html .= $messages->init($db);
+                $html .= $messages->init($db, $lang);
                 $html .= "</div>";
             }
             // settings TAB CONTENT
