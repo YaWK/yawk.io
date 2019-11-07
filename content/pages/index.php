e2d3fb307f2baf55dc4dd583c7ddea2d1847015b
dretzl <danielretzl@gmail.com>
2019-11-07 16:09:38 +0100

    updated new default index page

--- content/pages/index.php
+++ content/pages/index.php
@@ -1,18 +1,26 @@
-    
-    
-<div class="col-sm-12">
-        <h1>YAWK! :: Yet another WebKit</h1><br>  
-        <img src="media/images/deviceTypes.jpg" id="globe" class="img-fluid rounded float-right">
-    	<a href="http://yawk.io" target="_blank">YaWK</a> is a bundle of the most successful opensource web solutions. Weld together to build, design and operate any
-        modern website. The Backend got a bootstrap layout builder that allows you to design a versatile bootrap grid-based
-        layout with a just few clicks. Add and organize Pages, Articles, Menu Entries. Extend your page with big Plugins or 
-        small, useful widgets. Import your Twitch or YouTube Channel.<br><br>
+<div class="container-fluid">
+    <div class="row">
 
-    	<h2>Yet another Webkit</h2> is designed to be slick, fast and reliable. Steady in development.
-        Put together, what belongs together. YaWK bundles Bootstrap, jQuery, AdminLTE, FontAwesome and much more of your favourite 
-        "daily use" web(open)sourceware into a new atop overall CMS.
-    <small>small text</small> 
+        <div class="col-md-12 text-center">
+            <br><br>
+        	<h1 class="animated bounceInDown">
+                Hello World!
+            </h1>
+            <h2 class="animated fadeIn slow delay-1s">
+                This is a fresh Installation of Yet another Web Kit.<br>
+                <small class="text-muted animated fadeIn slow delay-2s">It's time to fill this place with creativity and love!</small>
+            </h2>
+            <br><br>
+            <h5 class="animated fadeIn slow delay-4s">
+                <i>Replace this with your own content.</i>
+            </h5>
+            <br><br>
+            <h6 class="animated fadeIn slow delay-5s">
+                If you need help, you may find it here: <br><br><i class="fa fa-github"></i> <a href="https://github.com/YaWK/yawk.io" target="_blank">YaWK on GitHub</a>
+            </h6>
+            <br><br>
+        </div>
+        
+    </div>
 </div>
-
- 
-                  
+                          
