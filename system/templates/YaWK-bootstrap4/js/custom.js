f10add03857f63aa77c3966a1cd5e7b1c1c4605d
YaWK <danielretzl@gmail.com>
2022-02-18 15:48:14 +0100

    Added smooth scrolling to anchor links & keep div in viewport JS code

--- system/templates/YaWK-bootstrap4/js/custom.js
+++ system/templates/YaWK-bootstrap4/js/custom.js
@@ -1,16 +1,47 @@
 $(document).ready(function () 
 {
+    // SMOOTH SCROLL TO TOP
+    // to activate smooth scroll from bottom to top SET class="scrollup" on the element you wish to send you upwards
     $('.scrollup').click(function () {
         $("html, body").animate({
             scrollTop: 0
         }, 600);
         return false;
     });
+    
+    // FADE OUT ANY ELEMENT ON SCROLL DOWN
+    // If you want to fade out an element on scroll down, set class="scrollDownFadeOut"
+    $(window).scroll(function() {
+  		$(".scrollDownFadeOut").css("opacity", 1 - $(window).scrollTop() / 1400);
+	});
+
+    // KEEP DIV IN VIEWPORT
+	// to keep any div box in the viewport after scrolling down, set class="scrollingDiv"
+    var $scrollingDiv = $("#scrollingDiv");
+    $(window).scroll(function(){            
+        $scrollingDiv
+            .stop()
+            .animate({"marginTop": ($(window).scrollTop() )}, 0);         
+    }); 
+    
+    // SMOOTH SCROLL TO ANCHORS
+    // if you want your internal links to smooth scroll, set class="sliding-link" target must be a div with id="yourtag"
+$(".sliding-link").click(function(e) {
+    e.preventDefault();
+    var aid = $(this).attr("href");
+    $('html,body').animate({scrollTop: $(aid).offset().top - 150},'slow');
+    window.location.hash = aid;
+});
 
     // set class="protected" to hide r.mouse context menu
-    $('.protected').bind('contextmenu', function(e) {
-        e.preventDefault();
-    });
+    /*
+    $("a[href^=#]").on('click', function(event) { 
+    event.preventDefault(); 
+    var name = $(this).attr('href'); 
+    var target = $('a[name="' + name.substring(1) + '"]'); 
+    $('html,body').animate({ scrollTop: $(target).offset().top }, 'slow'); 
+	});
+    */
 
     // auto adjust menu padding-top
     $(document.body).css('padding-top', $('#topnavbar').height() - 50);
