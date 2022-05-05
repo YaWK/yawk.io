3a6f47865bf22e8a07e099d3e49b97ff973e5dbe
YaWK <danielretzl@gmail.com>
2022-05-05 18:48:00 +0200

    issue #354: added use statement

--- system/setup/setup.php
+++ system/setup/setup.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
-// error_reporting(E_ALL ^ E_STRICT);                // show all erros - for development purpose only
+use YAWK\installer;
+// error_reporting(E_ALL ^ E_STRICT);                // show all errors - use this for development purpose only!
 // ALL ERRORS OFF - DEFAULT FOR PRODUCTION USE!
 error_reporting(0);
 // if installer is not set
 require_once('system/classes/installer.php');
 // create new object
-$installer = new \YAWK\installer();
+$installer = new installer();
 // initialize
 $installer->init();
