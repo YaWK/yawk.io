09058b3c5f712c775a776115ddfe0654f19ec36f
YaWK <danielretzl@gmail.com>
2022-06-06 14:36:16 +0200

    updated stats: get operating system array (added win 10/11)

--- system/classes/sys.php
+++ system/classes/sys.php
@@ -1042,6 +1042,9 @@ namespace YAWK {
 
             $os_platform    =   "Unknown OS Platform";
             $os_array       =   array(
+                'Sec-CH-UA-Platform'  =>  'Windows 11',
+                'Windows NT 10.0; Win64; x64'  =>  'Windows 10',
+                '/Windows NT 10.0'      =>  'Windows 10',
                 '/windows nt 6.2/i'     =>  'Windows 8',
                 '/windows nt 6.1/i'     =>  'Windows 7',
                 '/windows nt 6.0/i'     =>  'Windows Vista',
