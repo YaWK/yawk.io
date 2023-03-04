9df17c009dde0251f836baa66521e735b23381ec
YaWK <danielretzl@gmail.com>
2023-03-05 00:08:08 +0100

    #277: added comments to custom.js

--- system/templates/YaWK-bootstrap4/js/custom.js
+++ system/templates/YaWK-bootstrap4/js/custom.js
@@ -1,17 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function()
+{
+    // scroll to top method
+    // if you want to use this, add a div with class="scrollup" to your html element
     $('.scrollup').click(function() {
+        // this will smooth scroll to top of page
         $("html, body").animate({scrollTop:0}, 600);
         return false;
     });
+
+    // switch between dark and light mode
+    // set cookie to remember user choice
     $('#darkMode').click(function() {
         var id = $('#darkMode').data('id');
         document.cookie = 'frontendSwitchID=' + id;
     });
+    // on click of an element with id="lightMode", set cookie to remember user choice
     $('#lightMode').click(function() {
         var id = $('#lightMode').data('id');
         document.cookie = 'frontendSwitchID=' + id;
     });
 
+    // this fades out an element on scroll down
+    // this works well with images, like a carousel on top of the page that fades out on scroll down
+    // if you want to use this, add a div with class="scrollDownFadeOut" or add scrollDownFadeOut class to any element.
     $(window).scroll(function() {
         var screenWidth = $(window).width();
         var scrollTop = $(window).scrollTop();
@@ -19,11 +30,12 @@ $(document).ready(function() {
         $(".scrollDownFadeOut").css("opacity", fadeOutValue);
     });
 
-
+    // this will make a div sticky on scroll
     var $scrollingDiv = $("#scrollingDiv");
     $(window).scroll(function() {
         $scrollingDiv.stop().animate({"marginTop": ($(window).scrollTop())}, 0);
     });
+    // this will add smooth scrolling to all anchor links with class="sliding-link"
     $(".sliding-link").click(function(e) {
         e.preventDefault();
         var aid = $(this).attr("href");
@@ -31,7 +43,7 @@ $(document).ready(function() {
         window.location.hash = aid;
     });
 
-
+    // terminate user account (this must be refactored and moved to yawk's backend core js file)
     $('#terminateUser').click(function() {
         var terminate = window.confirm("ACHTUNG!\nDas wird Deinen Account permanent deaktivieren.\n" + "Bist Du Dir sicher, dass Du das tun willst?");
         if (terminate === true) {
@@ -47,20 +59,19 @@ $(document).ready(function() {
             }
         }
     });
+
+    // add bootstrap tooltip to all elements with data-toggle="tooltip"
     $(function() {
         $('[data-toggle="tooltip"]').tooltip();
     });
 
-    // import image compare viewer
+
+    // import single image compare viewer
     // import ImageCompare from "image-compare-viewer";
-    // get all image compare elements
-    // const viewers = document.querySelectorAll(".image-compare");
-    // mount all image compare elements
-
-
-
+    // Get the element
     //   const element = document.getElementById("image-compare");
     //   const viewer = new ImageCompare(element).mount();
+
     const options = {
         // UI Theme Defaults
         controlColor: "#FFFFFF",
@@ -71,23 +82,22 @@ $(document).ready(function() {
         // Label Defaults
         showLabels: true,
         labelOptions: {
-            before: 'Vorher',
-            after: 'Nachher',
+            before: 'Before',
+            after: 'After',
             onHover: true
         },
 
         // Smoothing
-
         smoothing: true,
         smoothingAmount: 80,
 
         // Other options
-
         hoverStart: true,
         verticalMode: false,
         startingPoint: 50,
         fluidMode: false
     };
+    // on each element with class="image-compare", create a new image compare viewer
     $('.image-compare').each(function() {
         let view = new ImageCompare(this, options).mount();
     });
