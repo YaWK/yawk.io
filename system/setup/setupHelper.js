63f59b50c3b1a149de9ede94ac4b91b6055c7fe9
Daniel Retzl <danielretzl@gmail.com>
2023-03-10 16:49:39 +0100

    #379: added form validation on step 4/5
    added validation for email syntax, username, password match

--- system/setup/setupHelper.js
+++ system/setup/setupHelper.js
@@ -5,7 +5,7 @@ $(document).ready(function() {  // wait until document is ready
 
 // This file is a helper to check the database credentials via xhr
 // Get the form element
-const form = document.getElementById('installerForm');
+    const form = document.getElementById('installerForm');
 // Add an event listener to the form submit event
     form.addEventListener('submit', (event) => {
         event.preventDefault(); // prevent the form from submitting
@@ -109,4 +109,178 @@ const form = document.getElementById('installerForm');
                 console.error(error);
             });
     });
-});
+//    $(document).ready(function() {  // wait until document is ready
+        $(function() {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+
+// This file is a helper to check the database credentials via xhr
+// Get the form element
+        const form = document.getElementById('installerForm');
+// Add an event listener to the form submit event
+        form.addEventListener('submit', (event) => {
+            event.preventDefault(); // prevent the form from submitting
+
+            // store save button in variable
+            var saveBtn = $("#savebutton");
+
+            // Get the form data
+            const formData = new FormData(form);
+
+            // Send an AJAX request to the server
+            fetch('system/setup/checkDatabaseCredentials.php', {
+                method: 'POST',
+                body: formData
+            })
+                // Get the response as JSON
+                .then(response => response.json()) // Parse the JSON response
+                .then(data => {
+                    // Handle the response data
+                    if (data.success)
+                    {   // Credentials are valid, continue with installation
+
+                        // indicate loading state, change button text and icon
+                        $(saveBtn).removeClass('btn btn-success').html('<small>2/5</small> '+data.DB_IMPORT_BTN+' &nbsp;<i class=\"fa fa-spinner fa-spin fa-fw\"></i>').addClass('btn btn-warning disabled');
+
+                        // submit form
+                        form.submit();
+
+                        // disable save button to prevent double submission if user clicks multiple times
+                        $(saveBtn).prop('disabled', true);
+
+                        // credentials are valid, display success message
+                        $.notify({
+                            // options
+                            title: '<h4><i class=\"fa fa-database\"></i>&nbsp; '+data.message+'</h4>',
+                            message: '<b>'+data.importmsg+'</b>',
+                        }, {
+                            // settings
+                            type: 'success',
+                            element: 'body',
+                            position: null,
+                            allow_dismiss: 'false',
+                            newest_on_top: 'true',
+                            placement: {
+                                from: 'top',
+                                align: 'center'
+                            },
+                            offset: {
+                                x: 10,
+                                y: 62
+                            },
+                            spacing: 10,
+                            z_index: 9999,
+                            delay: 6200,
+                            timer: 420,
+                            mouse_over: 'pause',
+                            animate: {
+                                enter: 'animated fadeInDown',
+                                exit: 'animated fadeOutUp'
+                            }
+                        });
+                    }
+                    else
+                    {   // Credentials are invalid, display error message
+                        $.notify({
+                            // options
+                            title: '<h4><i class=\"fa fa-database\"></i>&nbsp; '+data.message+'</h4>',
+                            message: '<b>'+data.subline+'</b>',
+                        }, {
+                            // settings
+                            type: 'danger',
+                            element: 'body',
+                            position: null,
+                            allow_dismiss: 'false',
+                            newest_on_top: 'true',
+                            placement: {
+                                from: 'top',
+                                align: 'center'
+                            },
+                            offset: {
+                                x: 10,
+                                y: 62
+                            },
+                            spacing: 10,
+                            z_index: 9999,
+                            delay: 4200,
+                            timer: 420,
+                            mouse_over: 'pause',
+                            animate: {
+                                enter: 'animated fadeInDown',
+                                exit: 'animated fadeOutUp'
+                            }
+                        });
+
+                        // change button text and icon
+                        $(saveBtn).html('2/5 '+data.checkagain+' &nbsp;<i id="savebuttonIcon" class="fa fa-refresh"></i>').removeClass().addClass('btn btn-success pull-right');
+                    }
+                })
+                .catch(error => {
+                    // Handle any errors that occurred during the request
+                    console.error(error);
+                });
+        });
+
+        // RUN AJAX TIMER
+        initTimer();
+
+        // set a timeout so that ajax calls will be delayed
+        function initTimer(){
+            var myTimer = 0;
+            $("#installerForm").on('keydown', function() {
+                // cancel any previously-set timer
+                if (myTimer) {
+                    clearTimeout(myTimer);
+                }
+
+                myTimer = setTimeout(function() {
+                    // call validation settings
+                    checkForm();
+                }, 200); //delay time in milliseconds
+            });
+        }
+
+        function checkForm(){
+            $('#installerForm').validate({ // initialize the plugin
+                errorPlacement: function(error, element) {
+                    error.insertBefore(element);
+                },
+                rules: {
+                    USERNAME: {
+                        required: true,
+                        minlength: 4,
+                        maxlength: 48,
+                        remote: {
+                            url: "system/plugins/signup/js/check-username.php",
+                            type: "post"
+                        }
+                    },
+                    EMAIL: {
+                        required: true,
+                        email: true,
+                        maxlength: 128
+                    },
+                    PASSWORD: {
+                        required: true,
+                        minlength: 4,
+                        maxlength: 48
+                    },
+                    PASSWORD2: {
+                        required: true,
+                        minlength: 4,
+                        maxlength: 48,
+                        equalTo: "#PASSWORD"
+                    }
+                },
+                messages: {
+                    username: {
+                        remote: "Please select another username. &nbsp;"
+                    },
+                    PASSWORD2: {
+                        equalTo: "Passwords do not match. &nbsp;"
+                    }
+                }
+            });
+        } // END function check form
+
+}); // END document ready
