401145ffeba21589dba871b0423d7dacaa4be08d
YaWK <danielretzl@gmail.com>
2023-03-15 23:02:42 +0100

    #387: added first version of readFilebase.php (called via ajax)

---
+++ admin/js/update-readFilebase.php
@@ -0,0 +1,22 @@
+<?php
+include '../../system/classes/db.php';
+include '../../system/classes/alert.php';
+include '../../system/classes/language.php';
+include '../../system/classes/settings.php';
+include '../../system/classes/update.php';
+/* set database object */
+if (!isset($db))
+{   // create new db object
+    $db = new \YAWK\db();
+}
+if (!isset($lang))
+{   // create new language object
+    $language = new YAWK\language();
+    // init language
+    $language->init($db, "backend");
+    // convert object param to array !important
+    $lang = (array) $language->lang;
+}
+// prepare vars
+$update = new \YAWK\update();
+$update->readFilebase($db, (array)$lang);
