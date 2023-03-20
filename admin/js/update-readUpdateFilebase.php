56cf6fd7710417f25140ff6d420fe6df2f8f1474
YaWK <danielretzl@gmail.com>
2023-03-20 02:21:24 +0100

    #389: add first version of array comparison (will be outsourced to another function)

--- admin/js/update-readUpdateFilebase.php
+++ admin/js/update-readUpdateFilebase.php
@@ -2,8 +2,37 @@
 use YAWK\update;
 include '../../system/classes/update.php';
 // prepare vars
+
+$localFilebase = parse_ini_file('../../system/update/filebase.current.ini');
+
 $update = new update();
 $updateFilebase = $update->readUpdateFilebaseFromServer();
+ksort($localFilebase);
+ksort($updateFilebase, SORT_NATURAL);
+
+$differentFiles = array();
+
+foreach ($localFilebase as $filePath => $localHash) {
+    if (isset($updateFilebase[$filePath])) {
+        $updateHash = $updateFilebase[$filePath];
+
+        if ($localHash !== $updateHash) {
+            $differentFiles[] = $filePath;
+        }
+    } else {
+        echo '<br><b>not found:</b> '.$filePath.' <span class="text-muted">in update filebase</span>';
+    }
+}
+
+if (empty($differentFiles)) {
+    echo "<b>All files have the same hash values.</b><br>";
+} else {
+    echo "<br><br><b>Files with different hash values:</b><br>";
+    foreach ($differentFiles as $file) {
+        echo "- $file<br>";
+    }
+}
+
+
 echo "<pre>";
-print_r($updateFilebase);
 echo "</pre>";
