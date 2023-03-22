cb46bf466814b57783bcaf93cb7b8cd1015db5bd
YaWK <danielretzl@gmail.com>
2023-03-23 00:48:23 +0100

    #389: improved array processing and $output view

--- admin/js/update-readUpdateFilebase.php
+++ admin/js/update-readUpdateFilebase.php
@@ -2,33 +2,83 @@
 use YAWK\update;
 include '../../system/classes/update.php';
 // prepare vars
-
+// get local (installed) filebase
 $localFilebase = parse_ini_file('../../system/update/filebase.current.ini');
 
+// generate new update object
 $update = new update();
+// get update filebase from server
 $updateFilebase = $update->readUpdateFilebaseFromServer();
+if (!is_array($updateFilebase)){
+    echo 'ERROR READING UPDATE FILEBASE FROM SERVER! This should be reachable: https://www.yawk.io/system/update/filebase.current.ini';
+}
+// sort arrays by key (file path)
 ksort($localFilebase);
 ksort($updateFilebase, SORT_NATURAL);
-
-$differentFiles = array();
-
-foreach ($localFilebase as $filePath => $localHash) {
-    if (isset($updateFilebase[$filePath])) {
+// compare filebase arrays
+// if file hash is different, add file to differentFiles array
+$differentFiles = array();  // files with different hash values (those will be updated)
+$localOnlyFiles = array();  // files only found in local filebase (files that were added by page admin, those will NOT be touched)
+// loop through local filebase array (parsed from filebase.current.ini)
+foreach ($localFilebase as $filePath => $localHash)
+{   // check if file exists in update filebase
+    if (isset($updateFilebase[$filePath]))
+    {   // get hash from update filebase
         $updateHash = $updateFilebase[$filePath];
-
-        if ($localHash !== $updateHash) {
+        // compare hashes
+        if ($localHash !== $updateHash)
+        {   // if hashes are different, add file to differentFiles array
             $differentFiles[] = $filePath;
         }
-    } else {
-        echo '<br><span class="animated fadeIn slow delay-2s"><b>not found:</b> '.$filePath.' <span class="text-muted">in update filebase</span></span>';
+    }
+    else
+    {   // if file is not found in update filebase, store file path in localOnlyFiles array
+        $localOnlyFiles[] = $filePath;
     }
 }
 
-if (empty($differentFiles)) {
-    echo '<p><b class="animated fadeIn slow delay-2s">All files have the same hash values.</b></p>';
-} else {
-    echo '<p class="animated fadeIn slow delay-3s"><br><b>Files with different hash values:</b></p>';
+$output = '';
+if (empty($differentFiles))
+{   // if no files with different hash values were found
+    $output = '<p><b class="animated fadeIn slow delay-2s">All files have the same hash values!!!</b></p>';
+}
+else
+{   // if files with different hash values were found
+    $output .= '<p class="animated fadeIn slow delay-3s"><br><b>Files with different hash values:</b></p>';
     foreach ($differentFiles as $file) {
-        echo '<span class="animated fadeIn slow delay-4s">- '.$file.'</span><br>';
+        $output .= '<span class="animated fadeIn slow delay-4s">- '.$file.'</span><br>';
     }
+}
+
+if (empty($localOnlyFiles)){
+    $output .= '<p><b class="animated fadeIn slow delay-2s">No files found that are not in the update filebase.</b></p>';
+}
+else
+{   // if files with different hash values were found
+    $output .= '<br><br><div class="panel-group animated fadeIn slow delay-3s" id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingOne">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Files that are not in the update filebase:<br>
+          <small>(those files will <u>not</u> be touched during update)</small>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+      <div class="panel-body">';
+    foreach ($localOnlyFiles as $file) {
+        $output .= '<span class="animated fadeIn slow">- '.$file.'</span><br>';
+    }
+    $output .= '
+      </div>
+    </div>
+  </div>';
+}
+
+if (!empty($output)){
+    echo $output;
+}
+else {
+    echo 'There was an error generating the view from no data...';
 }
