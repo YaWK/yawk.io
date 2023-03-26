d7bdc0463bc889e3de74f6aa27e41f5678e21666
YaWK <danielretzl@gmail.com>
2023-03-26 05:55:30 +0200

    #395: init language object, added newVersion and lang param to fetchFiles() method call

--- admin/js/update-fetchFiles.php
+++ admin/js/update-fetchFiles.php
@@ -1,7 +1,35 @@
 <?php
+use YAWK\db;
 use YAWK\update;
+include '../../system/classes/db.php';
+include '../../system/classes/language.php';
+include '../../system/classes/settings.php';
 include '../../system/classes/update.php';
+/* set database object */
+if (!isset($db))
+{   // create new db object
+    $db = new db();
+}
+if (!isset($lang))
+{   // create new language object
+    $language = new YAWK\language();
+    // init language
+    $language->init($db, "backend");
+    // convert object param to array !important
+    $lang = (array) $language->lang;
+}
+
 // prepare vars
-// generate new update object
-$update = new update();
-$update->fetchFiles();
+if (isset($_POST['updateVersion'])) {
+    $updateVersion = $_POST['updateVersion'];
+
+    // generate new update object
+    $update = new update();
+
+    // fetch update files
+    $update->fetchFiles($db, $updateVersion, $lang);
+}
+else
+{   // error
+    echo "Update Version could not be read!";
+}
