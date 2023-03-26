e985f9060c5c12d736579b0752ed986ecbc8eab8
YaWK <danielretzl@gmail.com>
2023-03-26 03:07:54 +0200

    #387: added use statements

--- admin/js/update-generateLocalFilebase.php
+++ admin/js/update-generateLocalFilebase.php
@@ -1,4 +1,6 @@
 <?php
+use YAWK\db;
+use YAWK\update;
 include '../../system/classes/db.php';
 include '../../system/classes/alert.php';
 include '../../system/classes/language.php';
@@ -7,7 +9,7 @@ include '../../system/classes/update.php';
 /* set database object */
 if (!isset($db))
 {   // create new db object
-    $db = new \YAWK\db();
+    $db = new db();
 }
 if (!isset($lang))
 {   // create new language object
@@ -18,5 +20,5 @@ if (!isset($lang))
     $lang = (array) $language->lang;
 }
 // prepare vars
-$update = new \YAWK\update();
+$update = new update();
 $update->generateLocalFilebase($db, (array)$lang);
