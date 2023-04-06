a0fd8f298d576277e4ad4446f6c2553576e9d1cf
YaWK <danielretzl@gmail.com>
2023-04-06 22:20:46 +0200

    #382: added Sidebar to displaySubMenu() method, added doc blocks

--- system/widgets/submenu/classes/submenu.php
+++ system/widgets/submenu/classes/submenu.php
@@ -19,30 +19,22 @@ namespace YAWK\WIDGETS\SUBMENU\EMBED
      */
     class submenu extends widget
     {
-        /** @param object global widget object data */
+        /** @var object global widget object data */
         public $widget = '';
-        /** @param string Title that will be shown above widget */
-        public $submenuHeading = '';
-        /** @param string Subtext will be displayed beside title */
-        public $submenuSubtext = '';
-        /** @param int ID of the menu to display */
-        public $menuID = '';
-
-        /** * @param string the class that is applied to the subMenu  */
-        public $subMenuClass;
-
-        /** * @param string the class that is applied to a subMenu Item  */
-        public $subMenuItemClass;
-
-        /**
-         * @brief display any subMenu (used by widgets to get any menu in any position)
-         * @copyright  2009-2016 Daniel Retzl
-         * @license    https://opensource.org/licenses/MIT
-         * @version    1.0.0
-         * @param object $db database
-         * @param int $menuID the menuID to get data
-         */
-
+        /** @var string Title that will be shown above widget */
+        public string $submenuHeading = '';
+        /** @var string Subtext will be displayed beside title */
+        public string $submenuSubtext = '';
+        /** @var string|int ID of the menu to display */
+        public string|int $menuID = '';
+        /** * @var string the class that is applied to the subMenu */
+        public string $subMenuClass;
+        /** * @var string the class that is applied to a subMenu Item */
+        public string $subMenuItemClass;
+        /** @var bool true|false display menu as sidebar   */
+        public string $sidebar = 'false';
+        /** @var bool true|false show menu name on top   */
+        public bool $showMenuName = false;
 
         /**
          * @brief Load all widget settings from database and fill object
@@ -57,6 +49,24 @@ namespace YAWK\WIDGETS\SUBMENU\EMBED
             foreach ($settings as $property => $value) {
                 $this->$property = $value;
             }
+
+            // set sidebar property
+            $this->sidebar = $this->widget->data['sidebar'];
+
+            // check if sidebar is set
+            if ($this->sidebar === 'true')
+            {   // include sidebar js
+                echo '<script src="system/widgets/submenu/sidebar.js" type="text/javascript"></script>';
+                // include sidebar css
+                echo '<link rel="stylesheet" href="system/widgets/submenu/sidebar.css">';
+            }
+
+            // check if menu obj is set
+            if (!isset($menu))
+            {
+                require_once 'system/classes/menu.php';
+                $menu = new \YAWK\menu();
+            }
         }
 
         /**
@@ -70,19 +80,54 @@ namespace YAWK\WIDGETS\SUBMENU\EMBED
             $this->drawMenu($db);
         }
 
+        /**
+         * @brief draw sidebar start
+         * @details If $this->sidebar is set to true, this function will be called before menu entry
+         * and wrap the menu in a sidebar div.
+         * @return void
+         */
+        public function drawSidebarStart(): void
+        {
+            echo '<div id="mySidebar" class="sidebar shadow"><a href="javascript:void(0)" class="btn btn-default" onclick="closeNav()">x</a>';
+        }
+
+        /**
+         * @brief draw sidebar end
+         * @details If $this->sidebar is set to true, this function will be called after menu entry
+         * and closes the wrapping menu sidebar div.
+         * @return void
+         */
+        public function drawSidebarEnd(): void
+        {
+            echo '</div>';
+        }
+
         public function drawMenu($db)
         {
-            // check if menu obj is set
-            if (!isset($menu))
-            {
-                require_once 'system/classes/menu.php';
-                $menu = new \YAWK\menu();
-            }
             $this->displaySubMenu($db, $this->menuID);
         }
 
+        /**
+         * @brief display any subMenu (used by widgets to get any menu in any position)
+         * @copyright  2009-2016 Daniel Retzl
+         * @license    https://opensource.org/licenses/MIT
+         * @version    1.0.0
+         * @param object $db database
+         * @param int $menuID the menuID to get data
+         */
         public function displaySubMenu($db, $menuID)
         {   /** @param db $db */
+
+            // check if menu name should be displayed
+            if ($this->showMenuName)
+            {   // get menu name from db
+                $menuName = "<li class=\"list-group-item\"><b>".\YAWK\menu::getMenuNameByID($db, $menuID)."</b><li>";
+            }
+            else
+            {   // don't show menu name
+                $menuName = '';
+            }
+
             // get menu entries and draw navbar
             $res = $db->query("SELECT * FROM {menu}
                                WHERE published = 1 
@@ -132,15 +177,28 @@ namespace YAWK\WIDGETS\SUBMENU\EMBED
                     $icon = "";
                 }
 
-                $subMenuItem .= '<li class="list-group-item">'.$icon.' &nbsp;&nbsp;<a href="'.$row['href'].'"'.$subMenuItemClass.$row['target'].'>'.$row['text'].'</a></li>';
+                // add menu item to menu
+                $subMenuItem .= '<li class="list-group-item hvr-grow"><small>'.$icon.'&nbsp;&nbsp;<a href="'.$row['href'].'"'.$subMenuItemClass.$row['target'].'>'.$row['text'].'</a></small></li>';
             }
+
+            // if sidebar is set, start sidebar markup
+            if ($this->sidebar === 'true') {   // include sidebar js
+                echo '<button class="openbtn" id="openSubmenuBtn">☰</button>';
+                // wrap menu in sidebar (start)
+                $this->drawSidebarStart();
+            }
+
+            // draw subMenu
             echo '<div id="subMenu"'.$subMenuClass.'>
-                    <ul class="list-group" style="cursor:pointer;">';
-            echo '    '.$subMenuItem.'</ul>
+                    <ul class="list-group" style="cursor:pointer;">'.$menuName.' ';
+            echo ' '.$subMenuItem.'</ul>
                   </div>';
+
+            // if sidebar is set, close sidebar
+            if ($this->sidebar === 'true')
+            {   // wrap menu in sidebar (end)
+                $this->drawSidebarEnd();
+            }
         }
-
-
     }
 }
-?>
