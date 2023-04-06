831ecbd0b36ecca4fa83dd0cb3aa81cf2aeb6ec1
YaWK <danielretzl@gmail.com>
2023-04-07 01:48:16 +0200

    #384: revert changes

--- system/classes/update.php
+++ system/classes/update.php
@@ -190,7 +190,7 @@ namespace YAWK
          * @details If update.ini contains migration files between the current version and the update version, this function will be called
          * @param $db object the database object
          * @param $lang array the language array
-         * @return void echo if migrations were successful or not
+         * @return bool true|false if migrations were successful or not
          */
         function runMigrations(object $db, array $lang): void
         {
@@ -386,13 +386,7 @@ namespace YAWK
             {
                 // updateFiles.ini exists
                 // parse updateFiles.ini into array
-                // $this->updateFiles = parse_ini_file($basedir.$this->localUpdateSystemPath . $this->updateFilesFile);
-                $updateFile = file_get_contents('https://update.yawk.io/filebase.ini');
-                if (!$updateFile) {
-                    die("Failed to fetch filebase.ini from update server!");
-                }
-                $this->updateFiles = parse_ini_string($updateFile, true);
-                // $this->updateFiles = parse_ini_file('https://update.yawk.io/filebase.ini');
+                $this->updateFiles = parse_ini_file($basedir.$this->localUpdateSystemPath . $this->updateFilesFile);
                 if (count($this->updateFiles) < 1)
                 {   // unable to read updateFiles.ini from local update folder
                     $response .= "<span class=\"text-warning\"><p><i class=\"fa fa-exclamation-triangle text-warning\"></i> &nbsp;No entries to process in updateFiles.ini - this can happen, if all of your files have the same hash value as the update files, but version number differs.<br><br><b>You may want to fast-forward to latest Version Number.</b><small><i>But think, before you click - and make a <a href=\"index.php?page=settings-update\">Backup!</a></i></small></p>
