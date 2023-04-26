d595c4153c669e11acedcc56c4a5f4606cf5080a
YaWK <danielretzl@gmail.com>
2023-04-26 17:18:30 +0200

    removed outdated static favicon links (will be replaced by method)

--- system/classes/AdminLTE.php
+++ system/classes/AdminLTE.php
@@ -124,10 +124,11 @@ namespace YAWK\BACKEND {
     <!-- Notify JS -->
     <script src=\"../system/engines/jquery/notify/bootstrap-notify.min.js\"></script>
         
-    <!-- favicons -->
+    <!-- favicons 
     <link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"apple-touch-icon.png\">
     <link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"favicon-32x32.png\">
     <link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"favicon-16x16.png\">
+    -->
     <!-- <link rel=\"manifest\" href=\"site.webmanifest\"> -->
     <link rel=\"mask-icon\" href=\"safari-pinned-tab.svg\" color=\"#000000\">
     <meta name=\"msapplication-TileColor\" content=\"#000000\">
